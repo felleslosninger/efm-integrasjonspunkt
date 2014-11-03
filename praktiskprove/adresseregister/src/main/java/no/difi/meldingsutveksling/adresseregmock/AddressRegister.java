03982b4349c9cc424a7901a51bd83e6efe6e58b3
glennbech <glenn.bech@gmail.com>
2014-11-03 16:29:27 +0100

    Adresseregister

---
+++ praktiskprove/adresseregister/src/main/java/no/difi/meldingsutveksling/adresseregmock/AddressRegister.java
@@ -0,0 +1,14 @@
+package no.difi.meldingsutveksling.adresseregmock;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: glennbech
+ * Date: 03.11.14
+ * Time: 16:16
+ * To change this template use File | Settings | File Templates.
+ */
+public interface AddressRegister {
+
+    String lookup(String orgNumber);
+
+}
---
+++ praktiskprove/adresseregister/src/main/java/no/difi/meldingsutveksling/adresseregmock/AdressRegisterFactory.java
@@ -0,0 +1,42 @@
+package no.difi.meldingsutveksling.adresseregmock;
+
+import java.util.HashMap;
+
+/**
+ * A very simple mock of an Lookup service providing public certificates
+ *
+ * @author Glenn Bech
+ */
+public class AdressRegisterFactory {
+
+    private static HashMap<String, String> keymap = new HashMap<String, String>();
+
+    static {
+        keymap.put("958935429",
+                "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDeLY9lI9jpDSTEjWkulWo9grWQ" +
+                        "0lIjq2EXpV4pjlo8jIAA4+z47oqYnmgMgDZFa+fG/V3HR+KMNikltMFIepDOj9RE" +
+                        "hpd/W5c8BZqugUTzoQ6lpj4o1qwKgzOrg3mASUp8YGp+D2dyNXkSaiAsOlQ67Jha" +
+                        "p0aZJxilYCGED00lkwIDAQAB");
+
+        keymap.put("960885406",
+                "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDO8NCBuAo+z6+yhww+HzNyxZc7" +
+                        "zeT47tm5zZ7A0YaEe8LCvscVVy3LJfpEnDcTSI8tvugWkfqZ4jOGwE6itX46zdqB" +
+                        "GejIpyExyc7kGKfzE4OPmnwleLyl5pJfrIqKY48Flg+2uBYT+k0H44HAKTyBH9Ys" +
+                        "KPG4IS9rUBsFI6ISxQIDAQAB");
+
+    }
+
+    public AddressRegister createAdressRegister() {
+        return new AddressRegister() {
+            @Override
+            public String lookup(String orgNumber) {
+
+                String key = keymap.get(orgNumber);
+                if (key == null) {
+                    throw new IllegalArgumentException("OrgNr " + orgNumber + " not found");
+                }
+                return key;
+            }
+        };
+    }
+}
