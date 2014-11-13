be4e5d665ea39da2ab0587d9aee94efa3ed8ec55
KubilayKa <Kubilay.Karayilan@inmeta.no>
2014-11-13 08:14:03 +0100

    MIIF-32

--- praktiskprove/eventlog/src/main/java/no/difi/meldingsutveksling/eventlog/Event.java
+++ praktiskprove/eventlog/src/main/java/no/difi/meldingsutveksling/eventlog/Event.java
@@ -1,8 +1,37 @@
 package no.difi.meldingsutveksling.eventlog;
 
+import java.util.Date;
 import java.util.UUID;
 
 public class Event {
     private UUID uuid;
+    private  int state;
+    private java.sql.Date timeStamp;
 
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public Event setUuid(UUID uuid) {
+        this.uuid = uuid;
+        return this;
+    }
+
+    public int getState() {
+        return state;
+    }
+
+    public Event setState(int state) {
+        this.state = state;
+        return this;
+    }
+
+    public java.sql.Date getTimeStamp() {
+        return timeStamp;
+    }
+
+    public Event setTimeStamp(java.sql.Date timeStamp) {
+        this.timeStamp = timeStamp;
+        return this;
+    }
 }
