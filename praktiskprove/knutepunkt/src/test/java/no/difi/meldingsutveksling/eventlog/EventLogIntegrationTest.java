89f3a890c5d88aefc884cdb1cd066d3fecc97b92
glennbech <glenn.bech@gmail.com>
2014-11-18 13:41:39 +0100

    MIIF-56 Api mot hendelseslog

--- praktiskprove/knutepunkt/src/test/java/no/difi/meldingsutveksling/eventlog/EventLogIntegrationTest.java
+++ praktiskprove/knutepunkt/src/test/java/no/difi/meldingsutveksling/eventlog/EventLogIntegrationTest.java
@@ -1,6 +1,6 @@
 package no.difi.meldingsutveksling.eventlog;
 
-import org.junit.Test;
+import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
@@ -12,6 +12,7 @@ import java.util.UUID;
  *
  */
 
+@Ignore
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"/spring-rest.xml"})
 
@@ -20,7 +21,6 @@ public class EventLogIntegrationTest {
     @Autowired
     private EventLogDAO dao;
 
-    @Test
     public void shouldInsert() {
         Event e = new Event().setUuid(UUID.randomUUID()).setProcessStates(ProcessState.AAPNINGS_KVITTERING_SENT).setSender("111111111").setReceiver("222222222");
         dao.insertEventLog(e);
