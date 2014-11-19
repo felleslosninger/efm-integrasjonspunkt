fb0ac6e88f70c7f83e9c264764af0663a54f84d6
kubkaray <kubi@inmeta.no>
2014-11-19 15:14:59 +0100

    MIIF-31 soap client

---
+++ praktiskprove/knutepunkt/src/test/java/no/difi/meldingsutveksling/oxalisexchange/NoarkWSClientTest.java
@@ -0,0 +1,16 @@
+package no.difi.meldingsutveksling.oxalisexchange;
+
+import org.junit.Test;
+
+
+/**
+ * Created by kubkaray on 19.11.2014.
+ */
+
+public class NoarkWSClientTest {
+    @Test
+    public void soapClientProbe() {
+        NoarkWSClient noarkWSClient = new NoarkWSClient();
+
+    }
+}
