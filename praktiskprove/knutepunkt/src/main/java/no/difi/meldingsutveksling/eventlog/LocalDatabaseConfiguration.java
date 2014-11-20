38b51f3a4c475ac33cbf7208b09187551f7a9ea1
glennbech <glenn.bech@gmail.com>
2014-11-20 16:26:20 +0100

    MIIF-55. Added support for local profile through system setting.

--- praktiskprove/knutepunkt/src/main/java/no/difi/meldingsutveksling/eventlog/LocalDatabaseConfiguration.java
+++ praktiskprove/knutepunkt/src/main/java/no/difi/meldingsutveksling/eventlog/LocalDatabaseConfiguration.java
@@ -2,10 +2,12 @@ package no.difi.meldingsutveksling.eventlog;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 @Configuration
+@Profile("dev")
 public class LocalDatabaseConfiguration implements DataBaseConfig {
 
     @Bean
