cf1b13917b6e7d0baecdb7cca7c32b64c677c289
Lars Erik Dahl <laerda@gmail.com>
2014-11-20 14:58:13 +0100

    MIIF-96 Knutepunkt responds with error if sender og receiver is not in Adresseregister

--- praktiskprove/knutepunkt/src/test/java/no/difi/meldingsutveksling/noarkexchange/SendMessageTemplateTest.java
+++ praktiskprove/knutepunkt/src/test/java/no/difi/meldingsutveksling/noarkexchange/SendMessageTemplateTest.java
@@ -1,26 +1,44 @@
 package no.difi.meldingsutveksling.noarkexchange;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 import java.io.IOException;
 import java.security.PrivateKey;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.CoreMatchers.*;
+import no.difi.meldingsutveksling.dokumentpakking.Dokumentpakker;
+import no.difi.meldingsutveksling.domain.Avsender;
+import no.difi.meldingsutveksling.domain.ByteArrayFile;
+import no.difi.meldingsutveksling.domain.Mottaker;
 import no.difi.meldingsutveksling.domain.SBD;
 import no.difi.meldingsutveksling.noarkexchange.schema.AddressType;
+import no.difi.meldingsutveksling.noarkexchange.schema.PutMessageRequestType;
+import no.difi.meldingsutveksling.services.AdresseregisterMock;
 
 import org.junit.Test;
 
 public class SendMessageTemplateTest {
 
-	SendMessageTemplate subject = new SendMessageTemplate() {
+	SendMessageTemplate subject = new SendMessageTemplate(new Dokumentpakker() {
+		@Override
+		public byte[] pakkDokumentISbd(ByteArrayFile document, Avsender avsender, Mottaker mottaker, String conversationId) {
+			return new byte[2];
+		}
+	}) {
+		{
+			setAdresseregister(new AdresseregisterMock());
+		}
 
 		@Override
 		void sendSBD(SBD sbd) throws IOException {
-			// Do nothing
 		}
 	};
 
-
+	@Test
+	public void testCreateSBD() {
+		subject.createSBD(new PutMessageRequestType(), new KnutepunktContext());
+	}
 
 	@Test
 	public void testVerifySender() throws Exception {
@@ -30,8 +48,8 @@ public class SendMessageTemplateTest {
 		subject.verifySender(adresseType, context);
 		assertThat(context.getAvsender(), is(notNullValue()));
 	}
-	
-	@Test(expected=IllegalArgumentException.class)
+
+	@Test(expected = IllegalArgumentException.class)
 	public void testVerifySenderInvalid() throws Exception {
 		AddressType adresseType = new AddressType();
 		adresseType.setOrgnr("12345678");
@@ -48,8 +66,8 @@ public class SendMessageTemplateTest {
 		subject.verifyRecipient(adresseType, context);
 		assertThat(context.getMottaker(), is(notNullValue()));
 	}
-	
-	@Test(expected=IllegalArgumentException.class)
+
+	@Test(expected = IllegalArgumentException.class)
 	public void testVerifyRecipientInvalid() throws Exception {
 		AddressType adresseType = new AddressType();
 		adresseType.setOrgnr("12345678");
@@ -57,7 +75,6 @@ public class SendMessageTemplateTest {
 		subject.verifyRecipient(adresseType, context);
 		assertThat(context.getMottaker(), is(notNullValue()));
 	}
-
 
 	@Test
 	public void testFindPrivateKey() throws Exception {
