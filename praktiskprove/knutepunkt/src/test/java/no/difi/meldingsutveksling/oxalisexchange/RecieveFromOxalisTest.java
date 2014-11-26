715d4b81578bc8d1847d51bfdf39db501e0dab3f
Lars Erik Dahl <laerda@gmail.com>
2014-11-26 10:11:10 +0100

    Ignore test that saves files to home folder

--- praktiskprove/knutepunkt/src/test/java/no/difi/meldingsutveksling/oxalisexchange/RecieveFromOxalisTest.java
+++ praktiskprove/knutepunkt/src/test/java/no/difi/meldingsutveksling/oxalisexchange/RecieveFromOxalisTest.java
@@ -1,5 +1,6 @@
 package no.difi.meldingsutveksling.oxalisexchange;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
@@ -7,6 +8,7 @@ import org.xml.sax.SAXException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+
 import java.io.File;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
@@ -15,7 +17,7 @@ import java.security.GeneralSecurityException;
  * Created by kubkaray on 20.11.2014.
  */
 public class RecieveFromOxalisTest {
-    @Test
+    @Test @Ignore
     public void recieve( ) {
         OxalisMessageReceiverTemplate oxalisMessageReceiverTemplate = new OxalisMessageReceiverTemplate();
         ClassLoader classLoader = getClass().getClassLoader();
