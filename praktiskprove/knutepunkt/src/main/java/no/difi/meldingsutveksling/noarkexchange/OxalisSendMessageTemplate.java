f8eef74a0d2c613c4e1f6ee3ed8ae2d97f3c4000
glennbech <glenn.bech@gmail.com>
2014-11-27 14:04:11 +0100

    ta ut sendregning

--- praktiskprove/knutepunkt/src/main/java/no/difi/meldingsutveksling/noarkexchange/OxalisSendMessageTemplate.java
+++ praktiskprove/knutepunkt/src/main/java/no/difi/meldingsutveksling/noarkexchange/OxalisSendMessageTemplate.java
@@ -2,12 +2,14 @@ package no.difi.meldingsutveksling.noarkexchange;
 
 import no.difi.meldingsutveksling.domain.SBD;
 import org.apache.commons.io.FileUtils;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.io.IOException;
 
 @Component
+@Profile("dev")
 public class OxalisSendMessageTemplate extends SendMessageTemplate {
 
     @Override
