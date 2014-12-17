418f18c09365a57f5e06a4734b02017c4c8d13a0
glennbech <glenn.bech@gmail.com>
2014-12-17 10:46:55 +0100

    '

---
+++ praktiskprove/adresseregister-web/src/main/java/no/difi/meldingsutveksling/adresseregister/AdresseRegisterController.java
@@ -0,0 +1,34 @@
+package no.difi.meldingsutveksling.adresseregister;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ Rest
+ */
+
+@RestController
+@RequestMapping("/adresseregister")
+public class AdresseRegisterController {
+
+    private AddressRegister adressRegister = AdressRegisterFactory.createAdressRegister(); // step 1 mock
+
+    @RequestMapping(value = "/{orgNr}/crt", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
+    public CertificateResponse getCertificate(@PathVariable(value = "organizationNumber") String orgNr) {
+        CertificateResponse response = new CertificateResponse();
+        response.setBase64EncondedCertificate(adressRegister.getCeritifcateString(orgNr));
+        return response;
+    }
+
+    public AddressRegister getAdressRegister() {
+        return adressRegister;
+    }
+
+    public void setAdressRegister(AddressRegister adressRegister) {
+        this.adressRegister = adressRegister;
+    }
+
+}
