74cc06c9be90259c1e1f2f679c25ceb91ecc0ceb
KubilayKarayilan <kkarayilan@yahoo.no>
2014-12-19 00:22:50 +0100

    MIIF-34

--- praktiskprove/knutepunkt/src/main/java/no/difi/meldingsutveksling/noarkexchange/OxalisSendMessageTemplate.java
+++ praktiskprove/knutepunkt/src/main/java/no/difi/meldingsutveksling/noarkexchange/OxalisSendMessageTemplate.java
@@ -30,7 +30,26 @@ public class OxalisSendMessageTemplate extends SendMessageTemplate {
 		TransmissionRequestBuilder requestBuilder = oxalisOutboundModule.getTransmissionRequestBuilder();
 
 		ByteArrayOutputStream os = new ByteArrayOutputStream();
-		MarshalSBD.marshal(sbd, os);
+   /*     //allerede marshaled # new ObjectFactory().createStandardBusinessDocument(doc) legger inn ny verdi?
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer  ;
+        try {
+            transformer = transformerFactory.newTransformer();
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ObjectOutputStream oos = new ObjectOutputStream(baos);
+            oos.writeObject(sbd);
+            oos.flush();
+            oos.close();
+            InputStream is = new ByteArrayInputStream(baos.toByteArray());
+            transformer.transform(new StreamSource(is), new StreamResult(outputStream));
+        } catch (TransformerConfigurationException e) {
+            throw new RuntimeException(e);
+        } catch (TransformerException e) {
+            e.printStackTrace();
+        }*/
+        MarshalSBD.marshal(sbd, os);
+
 
 		requestBuilder.payLoad(new ByteArrayInputStream(os.toByteArray()));
 		TransmissionRequest transmissionRequest = requestBuilder.build();
