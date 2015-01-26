c8d57682f9be08a564d5d960b35754ca85363054
glennbech <glenn.bech@gmail.com>
2015-01-26 15:06:18 +0100

    Modelmapper implementation that maps between generated and domain version of SBD.

--- praktiskprove/knutepunkt/src/main/java/no/difi/meldingsutveksling/noarkexchange/StandardBusinessDocumentFactory.java
+++ praktiskprove/knutepunkt/src/main/java/no/difi/meldingsutveksling/noarkexchange/StandardBusinessDocumentFactory.java
@@ -8,11 +8,11 @@ import no.difi.meldingsutveksling.domain.Mottaker;
 import no.difi.meldingsutveksling.domain.sbdh.StandardBusinessDocument;
 import no.difi.meldingsutveksling.noarkexchange.schema.ObjectFactory;
 import no.difi.meldingsutveksling.noarkexchange.schema.PutMessageRequestType;
+import org.modelmapper.ModelMapper;
 
-import javax.xml.bind.*;
-import javax.xml.namespace.QName;
-import javax.xml.transform.stream.StreamSource;
-import java.io.ByteArrayInputStream;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
 import java.io.ByteArrayOutputStream;
 import java.util.UUID;
 
@@ -22,12 +22,11 @@ import java.util.UUID;
  */
 public class StandardBusinessDocumentFactory {
 
-    public static final String NAMESPACE_URI = "http://www.unece.org/cefact/namespaces/StandardBusinessDocumentHeader";
-    public static final String NS_LOCAL = "ns";
-
     static StandardBusinessDocument create(PutMessageRequestType sender, Avsender avsender, Mottaker mottaker) {
-        Dokumentpakker pakker = new Dokumentpakker();
+        return create(sender, UUID.randomUUID().toString(), avsender, mottaker);
+    }
 
+    static StandardBusinessDocument create(PutMessageRequestType sender, String id, Avsender avsender, Mottaker mottaker) {
         final ByteArrayOutputStream os = new ByteArrayOutputStream();
         try {
             JAXBContext jaxbContext = JAXBContext.newInstance(PutMessageRequestType.class);
@@ -37,35 +36,17 @@ public class StandardBusinessDocumentFactory {
         } catch (JAXBException e) {
             throw new MeldingsUtvekslingRuntimeException(e);
         }
-        return pakker.pakk(new BestEduMessage(os.toByteArray()), avsender, mottaker, UUID.randomUUID().toString(), "melding");
+        return new Dokumentpakker().pakk(new BestEduMessage(os.toByteArray()), avsender, mottaker, UUID.randomUUID().toString(), "melding");
     }
 
     /**
-     * A practical solution to copy a generated StandardBusinessDocument to a domain version of the same class. Please note that this
-     * method is a bit expensice since it goes via XML to copy objects.
-     *
      * @param fromDocument
      * @return
      */
     static StandardBusinessDocument create(no.difi.meldingsutveksling.noarkexchange.schema.receive.StandardBusinessDocument fromDocument) {
-        try {
-            JAXBContext jaxbContextFrom = JAXBContext.newInstance(no.difi.meldingsutveksling.noarkexchange.schema.receive.StandardBusinessDocument.class);
-            JAXBContext jaxbContextTo = JAXBContext.newInstance(StandardBusinessDocument.class);
-            Marshaller jaxbMarshaller = jaxbContextFrom.createMarshaller();
-            Unmarshaller unMarshaller = jaxbContextTo.createUnmarshaller();
-            ByteArrayOutputStream os = new ByteArrayOutputStream();
-
-            JAXBElement<no.difi.meldingsutveksling.noarkexchange.schema.receive.StandardBusinessDocument> from =
-                    new JAXBElement<>(new QName(NAMESPACE_URI, NS_LOCAL), no.difi.meldingsutveksling.noarkexchange.schema.receive.StandardBusinessDocument.class, fromDocument);
-
-            jaxbMarshaller.marshal(from, os);
-            JAXBElement<StandardBusinessDocument> result = unMarshaller.unmarshal(new StreamSource(new ByteArrayInputStream(os.toByteArray())), StandardBusinessDocument.class);
-
-            return result.getValue();
-
-        } catch (JAXBException e) {
-            throw new MeldingsUtvekslingRuntimeException(e);
-        }
+        ModelMapper mapper = new ModelMapper();
+        return mapper.map(fromDocument, StandardBusinessDocument.class);
     }
+
 
 }
