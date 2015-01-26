9ca89b9298165f9cc9cca41925753c59c4e83d7e
glennbech <glenn.bech@gmail.com>
2015-01-26 15:10:20 +0100

    renamed knutepunkt to integrasjonspunkt

--- praktiskprove/domain/src/main/java/no/difi/meldingsutveksling/domain/sbdh/StandardBusinessDocumentHeader.java
+++ praktiskprove/domain/src/main/java/no/difi/meldingsutveksling/domain/sbdh/StandardBusinessDocumentHeader.java
@@ -8,12 +8,12 @@
 
 package no.difi.meldingsutveksling.domain.sbdh;
 
-import java.util.ArrayList;
-import java.util.List;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlType;
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -218,4 +218,11 @@ public class StandardBusinessDocumentHeader {
         this.businessScope = value;
     }
 
+    public void setSender(List<Partner> sender) {
+        this.sender = sender;
+    }
+
+    public void setReceiver(List<Partner> receiver) {
+        this.receiver = receiver;
+    }
 }
