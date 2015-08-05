98d95c905c8064002283e434221fb54386889349
Mads Hoel <mads.hoel@inmeta.no>
2015-08-05 16:14:30 +0200

    Split altinnmock project into altinnexhange and altinnmock projects. The altinnexchange now contains the WSDL and generation of Endpoints and clients.

---
+++ praktiskprove/transport-altinn/src/main/java/no/difi/meldingsutveksling/AltinnTransport.java
@@ -0,0 +1,11 @@
+package no.difi.meldingsutveksling;
+
+import no.difi.meldingsutveksling.domain.sbdh.StandardBusinessDocument;
+import no.difi.meldingsutveksling.transport.Transport;
+
+public class AltinnTransport implements Transport {
+    @Override
+    public void send(StandardBusinessDocument document) {
+
+    }
+}
