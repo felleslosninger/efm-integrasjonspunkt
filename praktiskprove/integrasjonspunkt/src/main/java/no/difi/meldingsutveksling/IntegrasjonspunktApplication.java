c7b92b57a6aea8d8131346fce8f7df75ff1ed5d1
dervism <dervis4@gmail.com>
2015-08-13 15:51:33 +0200

    Legger inn dependency for Spring Boot

---
+++ praktiskprove/integrasjonspunkt/src/main/java/no/difi/meldingsutveksling/IntegrasjonspunktApplication.java
@@ -0,0 +1,17 @@
+package no.difi.meldingsutveksling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Dervis M, 13/08/15.
+ */
+
+@SpringBootApplication
+public class IntegrasjonspunktApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntegrasjonspunktApplication.class, args);
+    }
+
+}
