c86add8d08f12dec4142811172fb0a60c15ffc7d
dervism <dervis4@gmail.com>
2015-08-14 12:22:53 +0200

    Legger til Spring boot admin client og nødvendig konfigurasjon for Eureka.

--- praktiskprove/integrasjonspunkt/src/main/java/no/difi/meldingsutveksling/IntegrasjonspunktApplication.java
+++ praktiskprove/integrasjonspunkt/src/main/java/no/difi/meldingsutveksling/IntegrasjonspunktApplication.java
@@ -4,6 +4,7 @@ import com.sun.xml.ws.transport.http.servlet.WSServletContextListener;
 import com.sun.xml.ws.transport.http.servlet.WSSpringServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.solr.SolrAutoConfiguration;
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
@@ -16,7 +17,7 @@ import javax.servlet.ServletException;
  * @author Dervis M, 13/08/15.
  */
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SolrAutoConfiguration.class})
 public class IntegrasjonspunktApplication extends SpringBootServletInitializer {
 
     @Override
