f72b3e2618a3da7c0835450a552b809cb5233fd4
dervism <dervis4@gmail.com>
2015-08-17 18:59:55 +0200

    Etterligner den gamle web.xml filen så mye som mulig.

--- praktiskprove/integrasjonspunkt/src/main/java/no/difi/meldingsutveksling/IntegrasjonspunktWSConfiguration.java
+++ praktiskprove/integrasjonspunkt/src/main/java/no/difi/meldingsutveksling/IntegrasjonspunktWSConfiguration.java
@@ -1,14 +1,16 @@
 package no.difi.meldingsutveksling;
 
 import com.sun.xml.ws.transport.http.servlet.WSSpringServlet;
+import org.springframework.boot.autoconfigure.web.DispatcherServletAutoConfiguration;
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.ws.config.annotation.WsConfigurerAdapter;
-import org.springframework.ws.transport.http.MessageDispatcherServlet;
 
 
 /**
@@ -28,4 +30,16 @@ public class IntegrasjonspunktWSConfiguration extends WsConfigurerAdapter{
         return reg;
     }
 
+    Etter@Bean
+    public DispatcherServlet dispatcherServlet() {
+        return new DispatcherServlet();
+    }
+
+    @Bean
+    public ServletRegistrationBean dispatcherServletRegistration() {
+        ServletRegistrationBean registration = new ServletRegistrationBean(dispatcherServlet(), "/api/*");
+        registration.setName(DispatcherServletAutoConfiguration.DEFAULT_DISPATCHER_SERVLET_REGISTRATION_BEAN_NAME);
+        registration.setLoadOnStartup(1);
+        return registration;
+    }
 }
