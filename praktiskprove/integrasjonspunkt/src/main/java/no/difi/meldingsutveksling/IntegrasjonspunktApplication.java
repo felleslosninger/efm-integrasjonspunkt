644fee17663e48090cf6ce354428926356fa295f
dervism <dervis4@gmail.com>
2015-08-18 10:43:26 +0200

    Tatt vekk EnableAutoConfiguration. Denne legger av en eller annen grunn Solr, som i sin tur gir feilaktig metric-info til spring boot admin.

--- praktiskprove/integrasjonspunkt/src/main/java/no/difi/meldingsutveksling/IntegrasjonspunktApplication.java
+++ praktiskprove/integrasjonspunkt/src/main/java/no/difi/meldingsutveksling/IntegrasjonspunktApplication.java
@@ -2,21 +2,17 @@ package no.difi.meldingsutveksling;
 
 import com.sun.xml.ws.transport.http.servlet.WSSpringServlet;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.solr.SolrAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.DispatcherServletAutoConfiguration;
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.DispatcherServlet;
 
 /**
  * @author Dervis M, 13/08/15.
  */
 
 @SpringBootApplication(exclude = {SolrAutoConfiguration.class})
-@EnableAutoConfiguration
 public class IntegrasjonspunktApplication extends SpringBootServletInitializer {
 
     @Bean
@@ -25,19 +21,6 @@ public class IntegrasjonspunktApplication extends SpringBootServletInitializer {
         ServletRegistrationBean reg = new ServletRegistrationBean(servlet, "/noarkExchange", "/receive");
         reg.setLoadOnStartup(1);
         return reg;
-    }
-
-    @Bean
-    public DispatcherServlet dispatcherServlet() {
-        return new DispatcherServlet();
-    }
-
-    @Bean
-    public ServletRegistrationBean dispatcherServletRegistration() {
-        ServletRegistrationBean registration = new ServletRegistrationBean(dispatcherServlet(), "/api/*");
-        registration.setName(DispatcherServletAutoConfiguration.DEFAULT_DISPATCHER_SERVLET_BEAN_NAME);
-        registration.setLoadOnStartup(1);
-        return registration;
     }
 
     public static void main(String[] args) {
