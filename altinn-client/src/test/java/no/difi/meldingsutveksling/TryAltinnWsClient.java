9f15344b0c597f370e98ac9724b8d5f69e9afe8a
Mads Hoel <mads.hoel@inmeta.no>
2015-09-03 16:27:03 +0200

    Updated mock to return a valid standard business document Fixed a bug in the unmarshaller Added test class for the converter

--- altinn-client/src/test/java/no/difi/meldingsutveksling/TryAltinnWsClient.java
+++ altinn-client/src/test/java/no/difi/meldingsutveksling/TryAltinnWsClient.java
@@ -1,5 +1,6 @@
 package no.difi.meldingsutveksling;
 
+import no.difi.meldingsutveksling.domain.sbdh.StandardBusinessDocument;
 import no.difi.meldingsutveksling.shipping.Request;
 
 import java.io.IOException;
@@ -26,7 +27,11 @@ public class TryAltinnWsClient {
         for (FileReference fileReference : result) {
             System.out.println(fileReference.getReceiptID());
             System.out.println(fileReference.getValue());
+
+            StandardBusinessDocument sbd = client.download(new DownloadRequest(fileReference.getValue(), request.getReceiver()));
+            System.out.println("downloaded sbd " + sbd);
         }
+
     }
 
     public static AltinnWsConfiguration configurationFromProperties() {
