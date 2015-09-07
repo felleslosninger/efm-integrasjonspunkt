557fe274a3748c2bb535f2916bcb240cbe94dec1
glennbech <glenn.bech@gmail.com>
2015-09-08 01:35:43 +0200

    Initial commit

--- transport-altinn/src/main/java/no/difi/meldingsutveksling/AltinnTransport.java
+++ transport-altinn/src/main/java/no/difi/meldingsutveksling/AltinnTransport.java
@@ -1,40 +1,91 @@
 package no.difi.meldingsutveksling;
 
+import no.difi.meldingsutveksling.domain.MeldingsUtvekslingRuntimeException;
+import no.difi.meldingsutveksling.domain.Organisasjonsnummer;
 import no.difi.meldingsutveksling.domain.sbdh.StandardBusinessDocument;
 import no.difi.meldingsutveksling.shipping.UploadRequest;
 import no.difi.meldingsutveksling.transport.Transport;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Properties;
+
+import static no.difi.meldingsutveksling.domain.Organisasjonsnummer.*;
+
+
+/**
+ * Transport implementation for Altinn message service.
+ *
+ * @author Glenn Bech
+ */
 public class AltinnTransport implements Transport {
-    public static void main(String[] args) {
-        new AltinnTransport().send(new StandardBusinessDocument());
-    }
 
-    // create zip from standard business document
-    // upload zip file (need to merge in Ftp branch)
-    // get receipt <-- ok
+    private Properties properties;
+
     @Override
-    public void send(StandardBusinessDocument document) {
-        UploadRequest request1 = new UploadRequest() {
+    public void send(final StandardBusinessDocument document) {
 
+        System.setProperty("com.sun.xml.internal.ws.transport.http.client.HttpTransportP‌​ipe.dump", "true");
+
+        properties=new Properties();
+        try (InputStream is = AltinnWsConfiguration.class.getResourceAsStream("/altinn.properties")) {
+            properties.load(is);
+        } catch (IOException e) {
+            throw new MeldingsUtvekslingRuntimeException(e);
+        }
+        final UploadRequest request1 = new UploadRequest() {
             @Override
             public String getSender() {
-                return "123456789";
+                Organisasjonsnummer orgNumberSender = fromIso6523(document.getStandardBusinessDocumentHeader().getSender().get(0).getIdentifier().getValue());
+                return orgNumberSender.toString();
             }
 
             @Override
             public String getReceiver() {
-                return "987654321";
+                Organisasjonsnummer orgNumberReceiver = fromIso6523(document.getStandardBusinessDocumentHeader().getReceiver().get(0).getIdentifier().getValue());
+                return orgNumberReceiver.toString();
             }
 
             @Override
             public String getSenderReference() {
-                return "my reference";
+                return String.valueOf(Math.random() * 3000);
             }
 
             @Override
             public StandardBusinessDocument getPayload() {
-                return new StandardBusinessDocument();
+                return document;
             }
         };
+
+        AltinnWsConfiguration config = configurationFromProperties();
+        AltinnWsClient client = new AltinnWsClient(config);
+        client.send(request1);
+    }
+
+    public AltinnWsConfiguration configurationFromProperties() {
+
+        AltinnWsConfiguration configuration;
+        URL brokerServiceUrl;
+        try {
+            brokerServiceUrl = new URL(properties.getProperty("altinn.brokerservice.url"));
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("Broker service url is wrong ", e);
+        }
+        URL streamingServiceUrl;
+        try {
+            streamingServiceUrl = new URL(properties.getProperty("altinn.streamingservice.url"));
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("Streaming service url is wrong ", e);
+        }
+        configuration = new AltinnWsConfiguration.Builder()
+                .withBrokerServiceUrl(brokerServiceUrl)
+                .withStreamingServiceUrl(streamingServiceUrl)
+                .withUsername(properties.getProperty("altinn.username"))
+                .withPassword(properties.getProperty("altinn.password"))
+                .build();
+
+        return configuration;
     }
 }
