b6f459f25343c6cc933f41eee92a5b6dc86df9f2
dervism <dervis4@gmail.com>
2015-09-18 09:50:03 +0200

    Start-filene for monitorering.

---
+++ monitorering/start-monitor-mac.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+# author: Dervis M
+# Builds and starts the monitoring server for the Integrasjonspunkt application
+# This is version should be used when running Mac.
+
+# Build parameters
+WORKING_DIR=$(pwd)
+SPRING_EUREKA=${WORKING_DIR}/integrasjonspunkt-eureka
+SPRING_ADMIN=${WORKING_DIR}/integrasjonspunkt-monitor
+
+# Trigger a Maven build if jar files 
+# does not exist.
+__jar() {
+    _JAR=$(ls $1/target/*jar)
+    if [ -z "$_JAR" ]; then
+        echo "There is no jar file in target folder."
+	mvn clean install -DskipTests=true
+    else
+       echo "Found $_JAR"
+    fi
+}
+
+# Check if jar files exits
+# Docker will not not build otherwise.
+__jar ${SPRING_EUREKA}
+__jar ${SPRING_ADMIN}
+
+# Build Docker-images and then start the containers.
+echo "Building and starting Spring Eureka Server"
+${SRING_EUREKA}/build-docker.sh
+
+echo "Building and starting Spring Boot Admin UI"
+${SPRING_ADMIN}/build-docker.sh 8090
