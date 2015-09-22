24216d070500a12c7d4c7fb81fee5664266931fe
glennbech <glenn.bech@gmail.com>
2015-09-22 10:28:06 +0200

    Fix for JAXB problem on the client

--- altinn-batch-receive/src/main/java/no/difi/meldingsutveksling/integrasjonspunkt/altinnreceive/ReceiveClient.java
+++ altinn-batch-receive/src/main/java/no/difi/meldingsutveksling/integrasjonspunkt/altinnreceive/ReceiveClient.java
@@ -1,10 +1,20 @@
 package no.difi.meldingsutveksling.integrasjonspunkt.altinnreceive;
 
+import com.sun.istack.internal.NotNull;
+import com.sun.xml.internal.bind.api.JAXBRIContext;
+import com.sun.xml.internal.bind.api.TypeReference;
+import com.sun.xml.internal.ws.api.model.SEIModel;
+import com.sun.xml.internal.ws.developer.JAXBContextFactory;
+import com.sun.xml.internal.ws.developer.UsesJAXBContextFeature;
+import no.difi.meldingsutveksling.dokumentpakking.xml.Payload;
 import no.difi.meldingsutveksling.noarkexchange.schema.receive.CorrelationInformation;
 import no.difi.meldingsutveksling.noarkexchange.schema.receive.SOAReceivePort;
 import no.difi.meldingsutveksling.noarkexchange.schema.receive.StandardBusinessDocument;
+import no.difi.meldingsutveksling.noarkexchange.schema.receive.StandardBusinessDocumentHeader;
 
+import javax.xml.bind.JAXBException;
 import javax.xml.ws.BindingProvider;
+import java.util.List;
 
 /**
  * This class is responsible for calleing the web service method "receive" on the
@@ -18,13 +28,34 @@ class ReceiveClient {
 
     public ReceiveClient(String endPointURL) {
         no.difi.meldingsutveksling.noarkexchange.schema.receive.Receive exchange = new no.difi.meldingsutveksling.noarkexchange.schema.receive.Receive();
-        port = exchange.getReceivePort();
+
+        UsesJAXBContextFeature jaxbContxtFeature = new UsesJAXBContextFeature(new JAXBContextFactory() {
+            @Override
+            public JAXBRIContext createJAXBContext(@NotNull SEIModel seiModel, @NotNull List<Class> classes, @NotNull List<TypeReference> typeReferences) throws JAXBException {
+                classes.add(Payload.class);
+                return JAXBContextFactory.DEFAULT.createJAXBContext(seiModel, classes, typeReferences);
+            }
+        });
+
+        port = exchange.getReceivePort(jaxbContxtFeature);
         BindingProvider bp = (BindingProvider) port;
         bp.getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, endPointURL);
     }
 
     public CorrelationInformation callReceive(StandardBusinessDocument sbd) {
         return port.receive(sbd);
+    }
+
+
+    public static void main(String[] args) {
+
+        StandardBusinessDocument sbd = new StandardBusinessDocument();
+        sbd.setStandardBusinessDocumentHeader(new StandardBusinessDocumentHeader());
+        sbd.setAny(new Payload(new byte[10]));
+
+        ReceiveClient rc = new ReceiveClient("http://localhost:8080/noarkExchange");
+        rc.callReceive(sbd) ;
+
     }
 
 }
