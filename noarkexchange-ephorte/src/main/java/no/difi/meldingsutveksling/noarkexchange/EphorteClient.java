eacf897be93646e4132227f885c5be2916cd40fe
Mads Hoel <mads.hoel@inmeta.no>
2015-10-15 14:52:01 +0200

    Added empty implementation of NoarkClient for ePhorte archive system

---
+++ noarkexchange-ephorte/src/main/java/no/difi/meldingsutveksling/noarkexchange/EphorteClient.java
@@ -0,0 +1,11 @@
+package no.difi.meldingsutveksling.noarkexchange;
+
+import no.difi.meldingsutveksling.noarkexchange.schema.PutMessageRequestType;
+import no.difi.meldingsutveksling.noarkexchange.schema.PutMessageResponseType;
+
+public class EphorteClient implements NoarkClient {
+    @Override
+    public PutMessageResponseType putMessage(PutMessageRequestType request) {
+        return null;
+    }
+}
