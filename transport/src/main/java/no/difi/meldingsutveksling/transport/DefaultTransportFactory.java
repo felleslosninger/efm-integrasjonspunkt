700c86970e36543e586d7e12e6ab319c2b9034aa
glennbech <glenn.bech@gmail.com>
2015-10-26 08:38:17 +0100

    Javadoc + removed public modifier to interface method in public interface

--- transport/src/main/java/no/difi/meldingsutveksling/transport/DefaultTransportFactory.java
+++ transport/src/main/java/no/difi/meldingsutveksling/transport/DefaultTransportFactory.java
@@ -3,15 +3,7 @@ package no.difi.meldingsutveksling.transport;
 import org.springframework.stereotype.Component;
 
 /**
- * The running instance of the "integrationspunkt" module may specify its own implementation of the TransportFactory.
- * This implementation is scans the classpath for transport.properties. The properties should contain one line
- * per content-type/transport class tuple.
- * <p/>
- * <pre>
- * transport=no.difi.meldingsutveksling.OxalisTransport
- * transport.urn:no:difi:meldingsutveksling:1.0=no.difi.meldingsutveksling.OxalisTransport
- * </pre>
- *
+ * The running instance of the "integrationspunkt" module should specify its own implementation of the TransportFactory.
  * @author Glenn Bech
  */
 @Component
@@ -19,10 +11,6 @@ public class DefaultTransportFactory implements TransportFactory {
 
     @Override
     public Transport createTransport(no.difi.meldingsutveksling.domain.sbdh.StandardBusinessDocument message) {
-        // try to find transport.properties on classpath
-        // try to find integrasjonspunkt.transport environment variable
-        // create nulltransport (Exception)
-
         return new NullTransport();
     }
 }
