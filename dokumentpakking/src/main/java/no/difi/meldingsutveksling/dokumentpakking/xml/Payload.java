ed045ae8fa2c8a67b156bdd670e6a52b30415cc2
Mads Hoel <mads.hoel@inmeta.no>
2016-01-12 13:29:23 +0100

    Bugfix: Optimize imports

--- dokumentpakking/src/main/java/no/difi/meldingsutveksling/dokumentpakking/xml/Payload.java
+++ dokumentpakking/src/main/java/no/difi/meldingsutveksling/dokumentpakking/xml/Payload.java
@@ -12,21 +12,21 @@ import java.nio.charset.Charset;
 @XmlRootElement(name = "payload", namespace = "urn:no:difi:meldingsutveksling:1.0")
 public class Payload {
 
-	public Payload(byte[] payload) {
-		this.Content =  new Content( new String(Base64.encodeBase64(payload),Charset.forName("UTF-8")));
-	}
-	public Payload() {
-		// Need no-arg constructor for JAXB
-	}
+    public Payload(byte[] payload) {
+        this.Content = new Content(new String(Base64.encodeBase64(payload), Charset.forName("UTF-8")));
+    }
+
+    public Payload() {
+        // Need no-arg constructor for JAXB
+    }
 //	
 
-	@XmlElement(namespace="urn:no:difi:meldingsutveksling:1.0")
-	private Content Content;
+    @XmlElement(namespace = "urn:no:difi:meldingsutveksling:1.0")
+    private Content Content;
 
-	public String getContent() {
-		return Content.getContent();
-	}
-
+    public String getContent() {
+        return Content.getContent();
+    }
 
 
 }
