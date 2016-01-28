dde917b433b6e3a2abc9110e0551306a49e124c6
Mads Hoel <mads.hoel@gmx.com>
2016-01-28 15:16:24 +0100

    Quickfix of new log bug

--- integrasjonspunkt/src/main/java/no/difi/meldingsutveksling/logging/MessageMarkerFactory.java
+++ integrasjonspunkt/src/main/java/no/difi/meldingsutveksling/logging/MessageMarkerFactory.java
@@ -38,7 +38,7 @@ public class MessageMarkerFactory {
         final LogstashMarker senderMarker = senderMarker(requestAdapter.getSenderPartynumber());
         final LogstashMarker conversationIdMarker = conversationIdMarker(requestAdapter.getConversationId());
         LogstashMarker logMarker = conversationIdMarker.and(receiverMarker).and(senderMarker);
-        if(requestAdapter.isAppReceipt()) {
+        if(!requestAdapter.isAppReceipt()) {
             LogstashMarker journalPostIdMarker = journalPostIdMarker(JournalpostId.fromPutMessage(requestAdapter).value());
             logMarker = logMarker.and(journalPostIdMarker);
         }
