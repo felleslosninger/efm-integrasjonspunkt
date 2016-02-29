96d97940a54b7fef353621849a1bf96535393c16
glennbech <glenn.bech@gmail.com>
2016-02-29 13:58:47 +0100

    Temporary fix for FIKS

---
+++ integrasjonspunkt/src/test/java/no/difi/meldingsutveksling/services/FiksFixtest.java
@@ -0,0 +1,19 @@
+package no.difi.meldingsutveksling.services;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FiksFixTest {
+
+    @Test
+    public void testShouldReplaceBergnsorgNumberWithKS() {
+        assertEquals("910639870", FiksFix.replaceOrgNummberWithKs("910951688"));
+    }
+
+    @Test
+    public void testShouldNotReplaceBergnsorgNumberWithKS() {
+        assertEquals("910075918", "910075918");
+    }
+
+}
