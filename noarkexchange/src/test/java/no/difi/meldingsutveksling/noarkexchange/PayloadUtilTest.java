4a5c77fd71240dc73adf346295b81dbb620b8971
Mads Hoel <mads.hoel@gmx.com>
2016-05-03 12:03:59 +0200

    #443 need testdata to reproduce problem in p360 client. Refactored private util method into separate test-tools module

--- noarkexchange/src/test/java/no/difi/meldingsutveksling/noarkexchange/PayloadUtilTest.java
+++ noarkexchange/src/test/java/no/difi/meldingsutveksling/noarkexchange/PayloadUtilTest.java
@@ -1,61 +1,42 @@
 package no.difi.meldingsutveksling.noarkexchange;
 
-import no.difi.meldingsutveksling.noarkexchange.schema.EnvelopeType;
 import no.difi.meldingsutveksling.noarkexchange.schema.PutMessageRequestType;
 import org.junit.Before;
 import org.junit.Test;
 
-import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
-import javax.xml.bind.Unmarshaller;
-import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.XMLStreamReader;
-import java.io.InputStream;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class PayloadUtilTest {
 
-    private JAXBContext ctx;
-    private Unmarshaller unmarshaller;
+    private TestData<PutMessageRequestType> testData;
 
     @Before
     public void setup() throws JAXBException {
-        ctx = JAXBContext.newInstance(PutMessageRequestType.class, EnvelopeType.class);
-        unmarshaller = ctx.createUnmarshaller();
+        testData = new TestData<>(PutMessageRequestType.class);
     }
 
     @Test
     public void isAppReceiptPutMessageFromEphorte() throws Exception {
-        PutMessageRequestType value = loadTestdataFromClasspath("ephorte/PutMessageMessage.xml");
+        PutMessageRequestType value = testData.loadFromClasspath("ephorte/PutMessageMessage.xml");
 
         assertFalse(PayloadUtil.isAppReceipt(value.getPayload()));
     }
 
     @Test
     public void isAppReceiptAppreceiptFrom360() throws JAXBException, XMLStreamException {
-        final PutMessageRequestType putMessageRequestType = loadTestdataFromClasspath("p360/PutMessageAppReceipt.xml");
+        final PutMessageRequestType putMessageRequestType = testData.loadFromClasspath("p360/PutMessageAppReceipt.xml");
 
         assertTrue(PayloadUtil.isAppReceipt(putMessageRequestType.getPayload()));
     }
 
     @Test
     public void isAppReceiptAppreceiptFromEphorte() throws JAXBException, XMLStreamException {
-        final PutMessageRequestType putMessageRequestType = loadTestdataFromClasspath("ephorte/PutMessageAppReceipt.xml");
+        final PutMessageRequestType putMessageRequestType = testData.loadFromClasspath("ephorte/PutMessageAppReceipt.xml");
 
         assertTrue(PayloadUtil.isAppReceipt(putMessageRequestType.getPayload()));
-    }
-
-    private PutMessageRequestType loadTestdataFromClasspath(String fileName) throws JAXBException, XMLStreamException {
-        InputStream file = this.getClass().getClassLoader().getResourceAsStream(fileName);
-
-        XMLInputFactory xif = XMLInputFactory.newFactory();
-        XMLStreamReader xsr = xif.createXMLStreamReader(file);
-        xsr.nextTag(); // Advance to Envelope tag
-        xsr.nextTag(); // Advance to Body tag
-        xsr.nextTag(); // Advance to getNumberResponse tag
-        return unmarshaller.unmarshal(xsr, PutMessageRequestType.class).getValue();
     }
 }
