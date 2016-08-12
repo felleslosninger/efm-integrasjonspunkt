d89ab536128100fde8a13d2531c6fba516e1a7a1
Mads Hoel <mads.hoel@gmx.com>
2016-08-12 13:48:38 +0200

    #502 Somehow this class was not indexed

---
+++ post-til-virksomhet/src/main/java/no/difi/meldingsutveksling/ptv/SoapFaultInterceptor.java
@@ -0,0 +1,78 @@
+package no.difi.meldingsutveksling.ptv;
+
+import net.logstash.logback.marker.LogstashMarker;
+import net.logstash.logback.marker.Markers;
+import no.difi.meldingsutveksling.logging.Audit;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ws.WebServiceMessage;
+import org.springframework.ws.client.WebServiceClientException;
+import org.springframework.ws.client.support.interceptor.ClientInterceptor;
+import org.springframework.ws.context.MessageContext;
+
+import javax.xml.transform.*;
+import javax.xml.transform.stream.StreamResult;
+import java.io.StringWriter;
+
+/**
+ * Used to log soap faults from Spring web service template
+ */
+public class SoapFaultInterceptor implements ClientInterceptor {
+    private LogstashMarker logMarkers;
+    private static final Logger logger = LoggerFactory.getLogger(SoapFaultInterceptor.class);
+
+    private SoapFaultInterceptor(LogstashMarker logMarkers) {
+        this.logMarkers = logMarkers;
+    }
+
+    /**
+     * Creates SoapFaultInterceptor that uses provided logmarkers when logging faults
+     * @param logMarkers the log markers to be used when logging
+     * @return new instance
+     */
+    public static SoapFaultInterceptor withLogMarkers(LogstashMarker logMarkers) {
+        return new SoapFaultInterceptor(logMarkers);
+    }
+
+    @Override
+    public boolean handleRequest(MessageContext messageContext) throws WebServiceClientException {
+        return true;
+    }
+
+    @Override
+    public boolean handleResponse(MessageContext messageContext) throws WebServiceClientException {
+        return true;
+    }
+
+    @Override
+    public boolean handleFault(MessageContext messageContext) throws WebServiceClientException {
+        throw new SoapFaultException("Failed to send message to correspondence agency");
+    }
+
+    class SoapFaultException extends WebServiceClientException {
+        public SoapFaultException(String msg) {
+            super(msg);
+        }
+    }
+
+    private String asString(Source source) {
+        StringWriter sw = new StringWriter();
+        try {
+            final Transformer transformer = TransformerFactory.newInstance().newTransformer();
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+            StreamResult sr = new StreamResult(sw);
+            transformer.transform(source, sr);
+            return sw.toString();
+        } catch (TransformerException e) {
+            logger.error(logMarkers, "Failed to marshall webservice response to XML string", e);
+        }
+        return "";
+    }
+
+    @Override
+    public void afterCompletion(MessageContext messageContext, Exception ex) throws WebServiceClientException {
+        final WebServiceMessage response = messageContext.getResponse();
+        Audit.error("Failed to send message to correspondence agency", logMarkers.and(Markers.append("soap_fault", asString(response.getPayloadSource()))), ex);
+    }
+}
