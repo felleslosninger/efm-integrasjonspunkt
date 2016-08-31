d2b809d924dfe0aed68aa41db8cbdae95f432aad
Nikolai Luthman <nikolai.luthman@inmeta.no>
2016-08-31 13:44:19 +0200

    Made build pipeline friendly
    - Fixed dockerfile
    - Added docker-maven-plugin
    - Added build info to logs
    - Secured logs sent to logstash
    - Fixed multiple spring profile bug
    - Adopted dependency management in root pom and child poms
    - Added docker specific spring profile
    - Include orgnumber spring profile
    - Removed peppol implementation
    - Added maven depedency check plugin (OWASP, vurnability db check)
    - Configured sonar
    - Added actuator security

--- integrasjonspunkt/src/test/java/no/difi/meldingsutveksling/noarkexchange/IntegrasjonspunktImplIntegrationTest.java
+++ integrasjonspunkt/src/test/java/no/difi/meldingsutveksling/noarkexchange/IntegrasjonspunktImplIntegrationTest.java
@@ -7,12 +7,12 @@ import no.difi.meldingsutveksling.noarkexchange.schema.PutMessageResponseType;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static org.junit.Assert.assertEquals;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 /**
  * Integration test class for {@link IntegrasjonspunktImpl}.
@@ -20,8 +20,7 @@ import static org.junit.Assert.assertEquals;
  * Mock overrides are configured in {@link IntegrasjonspunktIntegrationTestConfig}.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(IntegrasjonspunktApplication.class)
-@WebIntegrationTest
+@SpringBootTest(classes = IntegrasjonspunktApplication.class, webEnvironment = RANDOM_PORT)
 @ActiveProfiles("test")
 public class IntegrasjonspunktImplIntegrationTest {
 
