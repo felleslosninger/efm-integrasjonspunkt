c5c3331bd5b347c3331bb01187f25d5fefa0d9a8
Martin <martin.wam@difi.no>
2016-08-31 13:33:36 +0200

    #507 - local properties did not override profile settings. SR URI was also built wrong.

--- service-registry-client/src/main/java/no/difi/meldingsutveksling/serviceregistry/client/RestClient.java
+++ service-registry-client/src/main/java/no/difi/meldingsutveksling/serviceregistry/client/RestClient.java
@@ -2,6 +2,7 @@ package no.difi.meldingsutveksling.serviceregistry.client;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -36,7 +37,8 @@ public class RestClient {
      * @return response body
      */
     public String getResource(String resourcePath) {
-        URI uri =  baseUrl.resolve("/" + resourcePath);
+        URI uri = UriComponentsBuilder.fromUri(baseUrl).pathSegment(resourcePath).build().toUri();
+
         final ResponseEntity<String> entity = template.getForEntity(uri, String.class);
         return entity.getBody();
     }
