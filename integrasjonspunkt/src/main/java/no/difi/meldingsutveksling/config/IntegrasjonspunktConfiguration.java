0d1e011bbe9126584116e1b03627f76ef9583a0b
Nikolai Luthman <nikolai.luthman@inmeta.no>
2016-09-01 12:22:58 +0200

    Fixed application property loading to support multiple profiles

--- integrasjonspunkt/src/main/java/no/difi/meldingsutveksling/config/IntegrasjonspunktConfiguration.java
+++ integrasjonspunkt/src/main/java/no/difi/meldingsutveksling/config/IntegrasjonspunktConfiguration.java
@@ -25,12 +25,6 @@ public class IntegrasjonspunktConfiguration {
 
     @Configuration
     @Profile({"dev", "itest", "systest", "staging", "production", "test"})
-    @PropertySources(value = {
-            @PropertySource("classpath:properties/application.properties"),
-            @PropertySource("classpath:properties/integrasjonspunkt.properties"),
-            @PropertySource("classpath:properties/application-${spring.profiles.active}.properties"),
-            @PropertySource("classpath:properties/integrasjonspunkt-${spring.profiles.active}.properties")
-    })
     static class Default {
     }
 
