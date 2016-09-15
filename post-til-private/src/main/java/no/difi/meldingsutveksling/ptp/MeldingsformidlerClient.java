1ec74f7475bd3d3a46931b028668ba6bea38720c
Mads Hoel <mads.hoel@gmx.com>
2016-09-15 15:38:45 +0200

    #516 Cleanup of MeldingsformidlerClient Added TestDocument which is used by the manual test class MeldingsformilderClientMain

--- post-til-private/src/main/java/no/difi/meldingsutveksling/ptp/MeldingsformidlerClient.java
+++ post-til-private/src/main/java/no/difi/meldingsutveksling/ptp/MeldingsformidlerClient.java
@@ -8,7 +8,6 @@ import no.difi.sdp.client2.domain.exceptions.SendException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.UnsupportedEncodingException;
 import java.lang.invoke.MethodHandles;
 import java.security.KeyStore;
 import java.util.Date;
@@ -25,8 +24,7 @@ public class MeldingsformidlerClient {
     }
 
     public void sendMelding(MeldingsformidlerRequest request) throws MeldingsformidlerException {
-        KontaktInfo kontaktInfo = new KontaktInfo(); // this should come from SR once we're done
-        Mottaker mottaker = Mottaker.builder(request.getMottakerPid(), kontaktInfo.getPostkasseAdresse(), Sertifikat.fraByteArray(kontaktInfo.getCertificate()), kontaktInfo.getOrgnrPostkasse()).build();
+        Mottaker mottaker = Mottaker.builder(request.getMottakerPid(), request.getPostkasseAdresse(), Sertifikat.fraByteArray(request.getCertificate()), request.getOrgnrPostkasse()).build();
         DigitalPost digitalPost = DigitalPost.builder(mottaker, request.getSubject()).virkningsdato(new Date()).build();
         Dokument dokument = Dokument.builder(request.getDocumentTitle(), request.getDocumentName(), request.getDocument()).mimeType("application/vnd.openxmlformats-officedocument.wordprocessingml.document").build();
         Dokumentpakke dokumentpakke = Dokumentpakke.builder(dokument).build(); // skal dokumentpakke ha vedlegg?
@@ -88,22 +86,4 @@ public class MeldingsformidlerClient {
         }
     }
 
-    private class KontaktInfo {
-
-        public String getPostkasseAdresse() {
-            return "test.testesen#2HQF";
-        }
-
-        public byte[] getCertificate() {
-            try {
-                return "-----BEGIN CERTIFICATE-----\nMIIE7jCCA9agAwIBAgIKGBZrmEgzTHzeJjANBgkqhkiG9w0BAQsFADBRMQswCQYDVQQGEwJOTzEdMBsGA1UECgwUQnV5cGFzcyBBUy05ODMxNjMzMjcxIzAhBgNVBAMMGkJ1eXBhc3MgQ2xhc3MgMyBUZXN0NCBDQSAzMB4XDTE0MDQyNDEyMzA1MVoXDTE3MDQyNDIxNTkwMFowVTELMAkGA1UEBhMCTk8xGDAWBgNVBAoMD1BPU1RFTiBOT1JHRSBBUzEYMBYGA1UEAwwPUE9TVEVOIE5PUkdFIEFTMRIwEAYDVQQFEwk5ODQ2NjExODUwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQCLCxU4oBhtGmJxXZWbdWdzO2uA3eRNW/kPdddL1HYl1iXLV/g+H2Q0ELadWLggkS+1kOd8/jKxEN++biMmmDqqCWbzNdmEd1j4lctSlH6M7tt0ywmXIYdZMz5kxcLAMNXsaqnPdikI9uPJZQEL3Kc8hXhXISvpzP7gYOvKHg41uCxu1xCZQOM6pTlNbxemBYqvES4fRh2xvB9aMjwkB4Nz8jrIsyoPI89i05OmGMkI5BPZt8NTa40Yf3yU+SQECW0GWalB5cxaTMeB01tqslUzBJPV3cQx+AhtQG4hkOhQnAMDJramSPVtwbEnqOjQ+lyNmg5GQ4FJO02ApKJTZDTHAgMBAAGjggHCMIIBvjAJBgNVHRMEAjAAMB8GA1UdIwQYMBaAFD+u9XgLkqNwIDVfWvr3JKBSAfBBMB0GA1UdDgQWBBQ1gsJfVC7KYGiWVLP7ZwzppyVYTTAOBgNVHQ8BAf8EBAMCBLAwFgYDVR0gBA8wDTALBglghEIBGgEAAwIwgbsGA1UdHwSBszCBsDA3oDWgM4YxaHR0cDovL2NybC50ZXN0NC5idXlwYXNzLm5vL2NybC9CUENsYXNzM1Q0Q0EzLmNybDB1oHOgcYZvbGRhcDovL2xkYXAudGVzdDQuYnV5cGFzcy5uby9kYz1CdXlwYXNzLGRjPU5PLENOPUJ1eXBhc3MlMjBDbGFzcyUyMDMlMjBUZXN0NCUyMENBJTIwMz9jZXJ0aWZpY2F0ZVJldm9jYXRpb25MaXN0MIGKBggrBgEFBQcBAQR+MHwwOwYIKwYBBQUHMAGGL2h0dHA6Ly9vY3NwLnRlc3Q0LmJ1eXBhc3Mubm8vb2NzcC9CUENsYXNzM1Q0Q0EzMD0GCCsGAQUFBzAChjFodHRwOi8vY3J0LnRlc3Q0LmJ1eXBhc3Mubm8vY3J0L0JQQ2xhc3MzVDRDQTMuY2VyMA0GCSqGSIb3DQEBCwUAA4IBAQCe67UOZ/VSwcH2ov1cOSaWslL7JNfqhyNZWGpfgX1c0Gh+KkO3eVkMSozpgX6M4eeWBWJGELMiVN1LhNaGxBU9TBMdeQ3SqK219W6DXRJ2ycBtaVwQ26V5tWKRN4UlRovYYiY+nMLx9VrLOD4uoP6fm9GE5Fj0vSMMPvOEXi0NsN+8MUm3HWoBeUCLyFpe7/EPsS/Wud5bb0as/E2zIztRodxfNsoiXNvWaP2ZiPWFunIjK1H/8EcktEW1paiPd8AZek/QQoG0MKPfPIJuqH+WJU3a8J8epMDyVfaek+4+l9XOeKwVXNSOP/JSwgpOJNzTdaDOM+uVuk75n2191Fd7\n-----END CERTIFICATE-----\n".getBytes("UTF-8");
-            } catch (UnsupportedEncodingException e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-        public String getOrgnrPostkasse() {
-            return "984661185";
-        }
-    }
 }
--- post-til-private/src/main/java/no/difi/meldingsutveksling/ptp/MeldingsformidlerRequest.java
+++ post-til-private/src/main/java/no/difi/meldingsutveksling/ptp/MeldingsformidlerRequest.java
@@ -6,17 +6,66 @@ import java.io.InputStream;
  * Object to hold parameters used to send messages to sikker digital post
  */
 public interface MeldingsformidlerRequest {
+
+    /**
+     *
+     * @return the actual content of the post (mail)
+     */
     InputStream getDocument();
+
+    /**
+     *
+     * @return recipient person identifier
+     */
     String getMottakerPid();
+
+    /**
+     *
+     * @return subject in the post (mail)
+     */
     String getSubject();
 
+    /**
+     *
+     * @return typically the prefix in the filename
+     */
     String getDocumentName();
 
+    /**
+     *
+     * @return this title will be used to display the post in the target portal
+     */
     String getDocumentTitle();
 
+    /**
+     *
+     * @return virksomhetens organization number
+     */
     String getSenderOrgnumber();
 
+    /**
+     *
+     * @return an ID that uniquely identifies this message conversation. Must be in UIID format
+     */
     String getConversationId();
+
+    /**
+     *
+     * @return postkasse adresse as defined in KRR for the recipient person
+     */
+    String getPostkasseAdresse();
+
+    /**
+     *
+     * @return virksomhetssertifikat of the sending virksomhet
+     */
+    byte[] getCertificate();
+
+    /**
+     *
+     * @return the organization number of the postkasse provider as defined in KRR
+     */
+    String getOrgnrPostkasse();
 
     // === Optional fields ===
     /**
@@ -30,4 +79,5 @@ public interface MeldingsformidlerRequest {
      * @return
      */
     String getQueueId();
+
 }
