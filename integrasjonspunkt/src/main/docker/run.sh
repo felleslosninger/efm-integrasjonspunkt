ee9704ffd7cf942667639e788604a5f0c13f7fc9
Nikolai Luthman <nikolai.lutman@difi.no>
2016-11-15 15:15:19 +0100

    fixup! fixup! fixup! MIIF-144

--- integrasjonspunkt/src/main/docker/run.sh
+++ integrasjonspunkt/src/main/docker/run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-curl -s http://lb:9099/manage/health && \
-    curl -s http://lb:8761/discovery/manage/health  && \
-    dockerize -wait tcp://logstash:8300 && \
-    java -jar ${APP_JAVA_PARAMS} ${APP_DIR}/app.jar ${APP_MAIN_CLASS} --spring.profiles.active=${APP_PROFILE} "$@"
+# DEBUG this:
+#curl -s http://lb:9099/manage/health && \
+#    curl -s http://lb:8761/discovery/manage/health  && \
+#    dockerize -wait tcp://logstash:8300 && \
+ava -jar ${APP_JAVA_PARAMS} ${APP_DIR}/app.jar ${APP_MAIN_CLASS} --spring.profiles.active=${APP_PROFILE} "$@"
