e18d1aa1ae80e6b9e709fc846eb41a19ca14c62c
Mads Hoel <mads.hoel@gmx.com>
2016-11-25 17:02:02 +0100

    #move-97 Gson needed setters for aditional fields added. Street1-4 in PostAddress. Gson also needed empty constructor in PostAddress so the fields couldn't be final. Added return address to print forsendelse along with hardcoded values for: how to handle returned post, how to send post (a priority) and the color print (black/white). Need to add a task to have these configured via properties.

--- post-til-private/src/main/java/no/difi/meldingsutveksling/dpi/forsendelse/PrintForsendelseHandler.java
+++ post-til-private/src/main/java/no/difi/meldingsutveksling/dpi/forsendelse/PrintForsendelseHandler.java
@@ -7,8 +7,14 @@ import no.difi.sdp.client2.domain.AktoerOrganisasjonsnummer;
 import no.difi.sdp.client2.domain.Avsender;
 import no.difi.sdp.client2.domain.Dokumentpakke;
 import no.difi.sdp.client2.domain.Forsendelse;
+import no.difi.sdp.client2.domain.Sertifikat;
+import no.difi.sdp.client2.domain.TekniskMottaker;
 import no.difi.sdp.client2.domain.fysisk_post.FysiskPost;
 import no.difi.sdp.client2.domain.fysisk_post.KonvoluttAdresse;
+import no.difi.sdp.client2.domain.fysisk_post.Posttype;
+import no.difi.sdp.client2.domain.fysisk_post.Returhaandtering;
+import no.difi.sdp.client2.domain.fysisk_post.Utskriftsfarge;
+import no.digipost.api.representations.Organisasjonsnummer;
 
 public class PrintForsendelseHandler extends ForsendelseBuilderHandler {
     public PrintForsendelseHandler(DigitalPostInnbyggerConfig config) {
@@ -27,11 +33,21 @@ public class PrintForsendelseHandler extends ForsendelseBuilderHandler {
         } else {
             konvoluttAdresseHandler = new UtlandKonvoluttAdresseHandler();
         }
+        KonvoluttAdresseHandler returAdresseHandler;
+        if (request.getReturnAddress().isNorge()) {
+            returAdresseHandler = new NorgeKonvoluttAdresseHandler();
+        } else {
+            returAdresseHandler = new UtlandKonvoluttAdresseHandler();
+        }
 
         KonvoluttAdresse kon = konvoluttAdresseHandler.handle(request);
-        FysiskPost fysiskPost = FysiskPost.builder().adresse(kon).build();
+        // TODO property for utskriftsfarge, return strategy (shredding?), property for posttype dvs. a_prioritet eller b_økonomi
+        TekniskMottaker utskriftsleverandoer = new TekniskMottaker(Organisasjonsnummer.of(request.getOrgnrPostkasse()), Sertifikat.fraByteArray(request.getCertificate()));
+        FysiskPost fysiskPost = FysiskPost.builder().adresse(kon)
+                .retur(Returhaandtering.MAKULERING_MED_MELDING,
+                        returAdresseHandler.handle(request))
+                .sendesMed(Posttype.A_PRIORITERT).utskrift(Utskriftsfarge.SORT_HVIT, utskriftsleverandoer).build();
 
-        Forsendelse.fysisk(avsender, fysiskPost, dokumentpakke);
-        return null;
+        return Forsendelse.fysisk(avsender, fysiskPost, dokumentpakke);
     }
 }
--- service-registry-client/src/main/java/no/difi/meldingsutveksling/serviceregistry/externalmodel/PostAddress.java
+++ service-registry-client/src/main/java/no/difi/meldingsutveksling/serviceregistry/externalmodel/PostAddress.java
@@ -3,14 +3,20 @@ package no.difi.meldingsutveksling.serviceregistry.externalmodel;
 import com.google.common.collect.Sets;
 
 public class PostAddress {
-    private final String name;
-    private final String street1;
-    private final String street2;
-    private final String street3;
-    private final String street4;
-    private final String postalCode;
-    private final String postalArea;
-    private final String country;
+    private String name;
+    private String street1;
+    private String street2;
+    private String street3;
+    private String street4;
+    private String postalCode;
+    private String postalArea;
+    private String country;
+
+    public PostAddress() {
+        /**
+         * needed by json marshallar
+         */
+    }
 
     public PostAddress(String name, String street1, String street2, String street3, String street4, String postalCode, String postalArea, String country) {
         this.name = name;
@@ -54,6 +60,38 @@ public class PostAddress {
 
     public String getStreet4() {
         return street4;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setStreet1(String street1) {
+        this.street1 = street1;
+    }
+
+    public void setStreet2(String street2) {
+        this.street2 = street2;
+    }
+
+    public void setStreet3(String street3) {
+        this.street3 = street3;
+    }
+
+    public void setStreet4(String street4) {
+        this.street4 = street4;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public void setPostalArea(String postalArea) {
+        this.postalArea = postalArea;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
     }
 
     public boolean isNorge() {
