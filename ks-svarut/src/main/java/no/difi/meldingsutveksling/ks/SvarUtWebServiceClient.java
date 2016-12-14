989c83b292157bf17e9bb78b93e99b91ec11543c
Mads Hoel <mads.hoel@inmeta.no>
2016-12-14 12:24:57 +0100

    MOVE-122 Added bindings to add @XmlRootElement for JAXB generated classes needed to send message and get status from the service. Added method to get status for the sent message and added integration test for the using the WS client.

--- ks-svarut/src/main/java/no/difi/meldingsutveksling/ks/SvarUtWebServiceClient.java
+++ ks-svarut/src/main/java/no/difi/meldingsutveksling/ks/SvarUtWebServiceClient.java
@@ -2,36 +2,21 @@ package no.difi.meldingsutveksling.ks;
 
 import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
 
-import javax.activation.DataHandler;
-import javax.mail.util.ByteArrayDataSource;
-import javax.xml.bind.JAXBElement;
-
 public class SvarUtWebServiceClient extends WebServiceGatewaySupport {
-    public void sendMessage() {
-
-        byte[] data = new byte[1024];
-
-        final Dokument dokument = Dokument.builder().withData(new DataHandler(new ByteArrayDataSource(data, "pdf"))).build();
-
-        final Mottaker mottaker = Organisasjon.builder().build();
+    public String sendMessage(Forsendelse forsendelse) {
 
 
-        final Printkonfigurasjon printkonfigurasjon = Printkonfigurasjon.builder().build();
-        Forsendelse forsendelse = Forsendelse.builder()
-                .withTittel("Tittel")
-                .withAvgivendeSystem("Avgivende system?")
-                .withDokumenter(dokument)
-                .withMottaker(mottaker)
-                .withPrintkonfigurasjon(printkonfigurasjon)
-                .withKrevNiva4Innlogging(true)
-                .withKryptert(true).build();
 
-        final ObjectFactory objectFactory = new ObjectFactory();
         final SendForsendelse sendForsendelse = SendForsendelse.builder().withForsendelse(forsendelse).build();
 
-        final JAXBElement<SendForsendelseResponse> response = (JAXBElement<SendForsendelseResponse>) getWebServiceTemplate().marshalSendAndReceive(objectFactory.createSendForsendelse(sendForsendelse));
-        response.getValue();
+        final SendForsendelseResponse response = (SendForsendelseResponse) getWebServiceTemplate().marshalSendAndReceive(sendForsendelse);
 
-        //final ForsendelseStatus forsendelseStatus = (ForsendelseStatus) getWebServiceTemplate().marshalSendAndReceive(forsendelseId);
+        return response.getReturn();
+    }
+
+    public ForsendelseStatus getForsendelseStatus(String forsendelseId) {
+        RetrieveForsendelseStatus request = RetrieveForsendelseStatus.builder().withForsendelsesid(forsendelseId).build();
+        final RetrieveForsendelseStatusResponse response = (RetrieveForsendelseStatusResponse) getWebServiceTemplate().marshalSendAndReceive(request);
+        return response.getReturn();
     }
 }
--- ks-svarut/src/test/groovy/no/difi/meldingsutveksling/ks/SvarUtWebServiceClientTest.groovy
+++ ks-svarut/src/test/groovy/no/difi/meldingsutveksling/ks/SvarUtWebServiceClientTest.groovy
@@ -8,6 +8,8 @@ import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 import org.springframework.ws.test.client.MockWebServiceServer
 
+import javax.activation.DataHandler
+import javax.mail.util.ByteArrayDataSource
 import javax.xml.bind.JAXBContext
 import javax.xml.bind.util.JAXBSource
 
@@ -28,17 +30,43 @@ class SvarUtWebServiceClientTest {
     }
 
     @Test
-    void "SendMessage"() {
-        Forsendelse forsendelse = Forsendelse.builder().addDokumenter(Dokument.builder().build()).build()
-        //server.expect(payload(forsendelse)).andRespond("1234")
-        SendForsendelseResponse.builder().withReturn("123")
+    void "Testing to send message"() {
+        byte[] data = new byte[1024]
+
+        final Dokument dokument = Dokument.builder().withData(new DataHandler(new ByteArrayDataSource(data, "pdf"))).build()
+
+        final Mottaker mottaker = Organisasjon.builder().build()
+
+
+        final Printkonfigurasjon printkonfigurasjon = Printkonfigurasjon.builder().build()
+        Forsendelse forsendelse = Forsendelse.builder()
+                .withTittel("Tittel")
+                .withAvgivendeSystem("Avgivende system?")
+                .withDokumenter(dokument)
+                .withMottaker(mottaker)
+                .withPrintkonfigurasjon(printkonfigurasjon)
+                .withKrevNiva4Innlogging(true)
+                .withKryptert(true).build()
 
         SendForsendelseResponse payload = SendForsendelseResponse.builder().withReturn("123").build()
-        ObjectFactory objectFactory = new ObjectFactory()
         JAXBContext context = JAXBContext.newInstance(payload.getClass())
 
-        server.expect(anything()).andRespond(withPayload(new JAXBSource(context, objectFactory.createSendForsendelseResponse(payload))))
-        client.sendMessage()
+        server.expect(anything()).andRespond(withPayload(new JAXBSource(context, payload)))
+        client.sendMessage(forsendelse)
+
+        server.verify()
+    }
+
+    @Test
+    void "Testing to get status"() {
+        RetrieveForsendelseStatus request = RetrieveForsendelseStatus.builder().withForsendelsesid("123").build()
+
+        RetrieveForsendelseStatusResponse response = RetrieveForsendelseStatusResponse.builder().withReturn(ForsendelseStatus.MOTTATT).build()
+        JAXBContext context = JAXBContext.newInstance(response.getClass())
+        server.expect(anything()).andRespond(withPayload(new JAXBSource(context, response)))
+
+        client.getForsendelseStatus("123")
+
         server.verify()
     }
 }
