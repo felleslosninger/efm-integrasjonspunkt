8f3b6ced6e94c7d990ed4ca7cb491deb9286e77c
Mads Hoel <mads.hoel@inmeta.no>
2016-12-15 12:07:10 +0100

    MOVE-122 Added configuration files for KS SvarUt module.

--- ks-svarut/src/main/java/no/difi/meldingsutveksling/ks/SvarUtConfiguration.java
+++ ks-svarut/src/main/java/no/difi/meldingsutveksling/ks/SvarUtConfiguration.java
@@ -1,11 +1,17 @@
 package no.difi.meldingsutveksling.ks;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 
 @Configuration
+@EnableConfigurationProperties({SvarUtProperties.class})
 public class SvarUtConfiguration {
+    @Autowired
+    private SvarUtProperties properties;
+
     @Bean
     public Jaxb2Marshaller marshaller() {
         Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
--- ks-svarut/src/test/groovy/no/difi/meldingsutveksling/ks/SvarUtWebServiceClientTest.groovy
+++ ks-svarut/src/test/groovy/no/difi/meldingsutveksling/ks/SvarUtWebServiceClientTest.groovy
@@ -4,6 +4,7 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 import org.springframework.ws.test.client.MockWebServiceServer
@@ -17,6 +18,7 @@ import static org.springframework.ws.test.client.RequestMatchers.anything
 import static org.springframework.ws.test.client.ResponseCreators.withPayload
 
 @RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
 @ContextConfiguration(classes = [SvarUtConfiguration, SvarUtWebServiceTestConfiguration])
 class SvarUtWebServiceClientTest {
     @Autowired
