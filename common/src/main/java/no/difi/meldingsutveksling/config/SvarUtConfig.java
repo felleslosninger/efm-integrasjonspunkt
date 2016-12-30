3e05116ab553a90fcc0a38bfc56510be25532787
Mads Hoel <mads.hoel@inmeta.no>
2016-12-30 22:35:19 +0100

    MOVE-122 Added integration test for Svar Ut service: has the process from EDUCore -> Forsendelse (mapping + encryption) -> SvarUt Very few asserts added, but it will test cover all the wiring and any mappings to the actual message is sent via Soap

--- common/src/main/java/no/difi/meldingsutveksling/config/SvarUtConfig.java
+++ common/src/main/java/no/difi/meldingsutveksling/config/SvarUtConfig.java
@@ -5,8 +5,10 @@ import no.difi.meldingsutveksling.CertificateParser;
 import org.springframework.core.io.Resource;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
@@ -18,12 +20,21 @@ public class SvarUtConfig {
     @Valid()
     @Pattern(regexp = "^[a-zA-Z0-9\\-\\.øæåØÆÅ]{0,20}$")
     private String konverteringsKode;
+
+    @NotNull
     private Resource certificatePath;
 
 
     public X509Certificate getCertificate() {
+        final InputStream inputStream;
         try {
-            return new CertificateParser().parse(new InputStreamReader(certificatePath.getInputStream()));
+            inputStream = certificatePath.getInputStream();
+        } catch (IOException e) {
+            throw new ConfigException("Unable to open inputstream for certificate", e);
+        }
+
+        try {
+            return new CertificateParser().parse(new InputStreamReader(inputStream));
         } catch (IOException | CertificateException e) {
             throw new ConfigException("Unable to open certificate file", e);
         }
---
+++ ks-svarut/src/test/groovy/no/difi/meldingsutveksling/ks/SvarUtServiceIntegrationTest.groovy
@@ -0,0 +1,72 @@
+package no.difi.meldingsutveksling.ks
+
+import no.difi.meldingsutveksling.core.EDUCore
+import no.difi.meldingsutveksling.core.Receiver
+import no.difi.meldingsutveksling.core.Sender
+import no.difi.meldingsutveksling.noarkexchange.schema.core.DokumentType
+import no.difi.meldingsutveksling.noarkexchange.schema.core.FilType
+import no.difi.meldingsutveksling.noarkexchange.schema.core.JournpostType
+import no.difi.meldingsutveksling.noarkexchange.schema.core.MeldingType
+import no.difi.meldingsutveksling.noarkexchange.schema.core.NoarksakType
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+import org.springframework.ws.test.client.MockWebServiceServer
+
+import javax.xml.bind.JAXBContext
+import javax.xml.bind.util.JAXBSource
+
+import static org.springframework.ws.test.client.RequestMatchers.anything
+import static org.springframework.ws.test.client.ResponseCreators.withPayload
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = [SvarUtConfiguration, SvarUtIntegrationTestConfiguration])
+class SvarUtServiceIntegrationTest {
+    @Autowired
+    SvarUtService service
+
+    @Autowired
+    SvarUtWebServiceClientImpl client
+
+    MockWebServiceServer server
+
+    @Before
+    void setup() {
+        server = MockWebServiceServer.createServer(client)
+
+        SendForsendelseResponse payload = SendForsendelseResponse.builder().withReturn("123").build()
+        JAXBContext context = JAXBContext.newInstance(payload.getClass())
+        server.expect(anything()).andRespond(withPayload(new JAXBSource(context, payload)))
+    }
+
+    @Test
+    void "test send"() {
+        def eduCore = "an edu message"()
+
+
+        def forsendelseId = service.send(eduCore)
+        assert forsendelseId == "123"
+
+        server.verify()
+    }
+
+    def "an edu message"() {
+        EDUCore eduCore = new EDUCore()
+        eduCore.payload = new MeldingType(journpost:
+                new JournpostType(
+                        dokument: [new DokumentType(fil: new FilType(base64: [0x0,0x1,0x2]))]
+                ),
+                noarksak: new NoarksakType(saOfftittel: "Test brev")
+        )
+        eduCore.sender = new Sender(name: "Difi", identifier: "991825827")
+        eduCore.receiver = new Receiver(name: "Lote", identifier: "1234")
+        return eduCore
+    }
+
+
+}
