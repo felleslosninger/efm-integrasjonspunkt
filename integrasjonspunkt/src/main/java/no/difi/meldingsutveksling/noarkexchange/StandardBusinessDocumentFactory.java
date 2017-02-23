3d103911466f020c207c77fd3e5ae39c4aa93b3a
Jenkins <jenkins@localhost>
2017-02-23 13:40:27 +0100

    [RELEASE]squashing 'feature-MOVE-191_nsprefix' into 'development'

--- integrasjonspunkt/src/main/java/no/difi/meldingsutveksling/noarkexchange/StandardBusinessDocumentFactory.java
+++ integrasjonspunkt/src/main/java/no/difi/meldingsutveksling/noarkexchange/StandardBusinessDocumentFactory.java
@@ -81,6 +81,10 @@ public class StandardBusinessDocumentFactory {
     public EduDocument create(EDUCore shipment, String conversationId, Avsender avsender, Mottaker mottaker) throws MessageException {
         EDUCoreConverter eduCoreConverter = new EDUCoreConverter();
         byte[] marshalledShipment = eduCoreConverter.marshallToBytes(shipment);
+        if (shipment.getMessageType() == EDUCore.MessageType.APPRECEIPT) {
+            // MOVE-191: Need to remove ns prefix due to it giving errors in ephorte
+            marshalledShipment = new String(marshalledShipment).replaceAll(":ns2|ns2:", "").getBytes();
+        }
 
         BestEduMessage bestEduMessage = new BestEduMessage(marshalledShipment);
         LogstashMarker marker = markerFrom(shipment);
