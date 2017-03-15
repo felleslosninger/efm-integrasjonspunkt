0e1d084212c2466d62c5d1edb32533b36a845830
Jenkins <jenkins@localhost>
2017-03-15 17:10:19 +0100

    [RELEASE]squashing 'feature-MOVE-205_nextbest_logging' into 'development'

---
+++ nextbest/src/main/java/no/difi/meldingsutveksling/nextbest/logging/ConversationResourceMarkers.java
@@ -0,0 +1,25 @@
+package no.difi.meldingsutveksling.nextbest.logging;
+
+import net.logstash.logback.marker.LogstashMarker;
+import net.logstash.logback.marker.Markers;
+import no.difi.meldingsutveksling.nextbest.ConversationResource;
+
+public class ConversationResourceMarkers {
+
+    private static final String CONVERSATION_ID = "conversation_id";
+    private static final String SENDER_ID = "sender_id";
+    private static final String RECEIVER_ID = "receiver_id";
+    private static final String MESSAGETYPE_ID = "messagetype_id";
+
+    private ConversationResourceMarkers() {
+    }
+
+    public static LogstashMarker markerFrom(ConversationResource resource) {
+
+        final LogstashMarker conversationIdMarker = Markers.append(CONVERSATION_ID, resource.getConversationId());
+        final LogstashMarker senderIdMarker = Markers.append(SENDER_ID, resource.getSenderId());
+        final LogstashMarker receiverMarker = Markers.append(RECEIVER_ID, resource.getReceiverId());
+        final LogstashMarker messagetypeIdMarker = Markers.append(MESSAGETYPE_ID, resource.getMessagetypeId());
+        return conversationIdMarker.and(senderIdMarker).and(receiverMarker).and(messagetypeIdMarker);
+    }
+}
