a113056e305d25234918e8f9a7491d5ece768899
Mads Hoel <mads.hoel@gmx.com>
2017-03-16 13:31:28 +0100

    #move-131 Added mapping of conversation id to forsendelse external reference along with missing metadata mapping. Removed most of the old over-engineered mapping. Remaining: [] Map conversation id to forsendelse id (so that we poll on the correct message transaction) [] Map forsendelse status to conversation status [] Close/finnish conversation

--- ks-svarut/src/main/java/no/difi/meldingsutveksling/ks/mapping/ForsendelseMapper.java
+++ ks-svarut/src/main/java/no/difi/meldingsutveksling/ks/mapping/ForsendelseMapper.java
@@ -1,33 +1,32 @@
 package no.difi.meldingsutveksling.ks.mapping;
 
-import no.difi.meldingsutveksling.config.FiksConfig;
 import no.difi.meldingsutveksling.config.IntegrasjonspunktProperties;
 import no.difi.meldingsutveksling.core.EDUCore;
-import no.difi.meldingsutveksling.core.Receiver;
 import no.difi.meldingsutveksling.ks.*;
 import no.difi.meldingsutveksling.ks.mapping.edu.FileTypeHandler;
 import no.difi.meldingsutveksling.ks.mapping.edu.FileTypeHandlerFactory;
 import no.difi.meldingsutveksling.noarkexchange.schema.core.DokumentType;
 import no.difi.meldingsutveksling.noarkexchange.schema.core.MeldingType;
+import no.difi.meldingsutveksling.serviceregistry.ServiceRegistryLookup;
+import no.difi.meldingsutveksling.serviceregistry.externalmodel.InfoRecord;
 
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
 import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ForsendelseMapper {
     private IntegrasjonspunktProperties properties;
-    FiksConfig fiksConfig;
-    private X509Certificate certificate;
-    private FileTypeHandlerFactory fileTypeHandlerFactory;
+    private ServiceRegistryLookup serviceRegistry;
 
-    public ForsendelseMapper(IntegrasjonspunktProperties properties, FiksConfig fiksConfig, X509Certificate certificate) {
+    public ForsendelseMapper(IntegrasjonspunktProperties properties, ServiceRegistryLookup serviceRegistry) {
         this.properties = properties;
-        this.fiksConfig = fiksConfig;
-        this.certificate = certificate;
-        this.fileTypeHandlerFactory = new FileTypeHandlerFactory(fiksConfig, certificate);
+        this.serviceRegistry = serviceRegistry;
     }
 
-    Forsendelse mapFrom(EDUCore eduCore) {
+    public Forsendelse mapFrom(EDUCore eduCore, X509Certificate certificate) {
         final Forsendelse.Builder<Void> builder = Forsendelse.builder();
         builder.withEksternref(eduCore.getId());
         builder.withKunDigitalLevering(true);
@@ -35,16 +34,20 @@ public class ForsendelseMapper {
         final MeldingType meldingType = eduCore.getPayloadAsMeldingType();
         builder.withTittel(meldingType.getNoarksak().getSaOfftittel());
 
-        builder.withDokumenter(mapFrom(meldingType.getJournpost().getDokument()));
+        final FileTypeHandlerFactory fileTypeHandlerFactory = new FileTypeHandlerFactory(properties.getFiks(), certificate);
+        builder.withDokumenter(mapFrom(meldingType.getJournpost().getDokument(), fileTypeHandlerFactory));
 
-        builder.withKonteringskode(fiksConfig.getUt().getKonverteringsKode());
-        builder.withKryptert(fiksConfig.isKryptert());
+        builder.withKonteringskode(properties.getFiks().getUt().getKonverteringsKode());
+        builder.withKryptert(properties.getFiks().isKryptert());
         builder.withAvgivendeSystem(properties.getNoarkSystem().getType());
 
-        builder.withMottaker(mottakerFrom(eduCore.getReceiver()));
+        final InfoRecord receiverInfo = serviceRegistry.getInfoRecord(eduCore.getReceiver().getIdentifier());
+        builder.withMottaker(mottakerFrom(receiverInfo));
 
-        builder.withMetadataFraAvleverendeSystem(metaDataFrom())
+        final InfoRecord senderInfo = serviceRegistry.getInfoRecord(eduCore.getSender().getIdentifier());
+        builder.withSvarSendesTil(mottakerFrom(senderInfo));
 
+        builder.withMetadataFraAvleverendeSystem(metaDataFrom(meldingType));
 
         return builder.build();
     }
@@ -56,26 +59,35 @@ public class ForsendelseMapper {
         metadata.withSaksaar(Integer.valueOf(meldingType.getNoarksak().getSaSaar()));
         metadata.withJournalaar(Integer.valueOf(meldingType.getJournpost().getJpJaar()));
         metadata.withJournalsekvensnummer(Integer.valueOf(meldingType.getJournpost().getJpSeknr()));
-        metadata.with
+        metadata.withJournalpostnummer(Integer.valueOf(meldingType.getJournpost().getJpJpostnr()));
+        metadata.withJournalposttype(meldingType.getJournpost().getJpNdoktype());
+        metadata.withJournalstatus(meldingType.getJournpost().getJpStatus());
+        metadata.withJournaldato(journalDatoFrom(meldingType.getJournpost().getJpDokdato()));
 
-
-        return null;
+        return metadata.build();
     }
 
-    private Mottaker mottakerFrom(Receiver receiver) {
+    private XMLGregorianCalendar journalDatoFrom(String jpDokdato) {
+        try {
+            return DatatypeFactory.newInstance().newXMLGregorianCalendar(jpDokdato);
+        } catch (DatatypeConfigurationException e) {
+            throw new ForsendelseMappingException("Unable to map journalDato", e);
+        }
+    }
+
+    private Mottaker mottakerFrom(InfoRecord infoRecord) {
         Organisasjon.Builder<Void> mottaker = Organisasjon.builder();
-        mottaker.withOrgnr(receiver.getIdentifier());
+        mottaker.withOrgnr(infoRecord.getIdentifier());
 
         mottaker.withPostnr("0192");
         mottaker.withPoststed("Oslo");
-        mottaker.withNavn("Navn");
+        mottaker.withNavn(infoRecord.getOrganizationName());
         return mottaker.build();
     }
 
-    List<Dokument> mapFrom(List<DokumentType> dokumentTypes) {
+    private List<Dokument> mapFrom(List<DokumentType> dokumentTypes, FileTypeHandlerFactory fileTypeHandlerFactory) {
         List<Dokument> dokumenter = new ArrayList<>(dokumentTypes.size());
         for (DokumentType d : dokumentTypes) {
-            FileTypeHandlerFactory fileTypeHandlerFactory = this.fileTypeHandlerFactory;
             final FileTypeHandler fileTypeHandler = fileTypeHandlerFactory.createFileTypeHandler(d);
             Dokument.Builder dokumentBuilder =  fileTypeHandler.map(Dokument.builder());
 
