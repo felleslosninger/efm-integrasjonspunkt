199a2659945363e917762bb6fea5c0d653fab287
Jenkins <jenkins@localhost>
2017-08-01 15:19:39 +0200

    [RELEASE]squashing 'feature-MOVE-283_nextmove_dpi' into 'development'

--- fiks-meldingsformidler/src/main/java/no/difi/meldingsutveksling/ks/svarinn/MimeTypeExtensionMapper.java
+++ fiks-meldingsformidler/src/main/java/no/difi/meldingsutveksling/ks/svarinn/MimeTypeExtensionMapper.java
@@ -1,17 +1,15 @@
 package no.difi.meldingsutveksling.ks.svarinn;
 
-import com.google.common.collect.Maps;
+import com.google.common.collect.HashBiMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
-
-import java.util.HashMap;
 
 public class MimeTypeExtensionMapper {
 
     private static final Logger log = LoggerFactory.getLogger(MimeTypeExtensionMapper.class);
 
-    private static final HashMap<String, String> mimeTypeMap = Maps.newHashMap();
+    private static final HashBiMap<String, String> mimeTypeMap = HashBiMap.create();
 
     static {
         mimeTypeMap.put("application/msword", "DOC");
@@ -28,6 +26,12 @@ public class MimeTypeExtensionMapper {
     private MimeTypeExtensionMapper() {
     }
 
+    /**
+     * Returns extension based on mimeType.
+     *
+     * @param mimeType the mime type to map
+     * @return the mapped extension, or "PDF" as default if no mapping was found
+     */
     public static String getExtension(String mimeType) {
         if (mimeTypeMap.containsKey(mimeType)) {
             return mimeTypeMap.get(mimeType);
@@ -35,5 +39,20 @@ public class MimeTypeExtensionMapper {
 
         log.error(String.format("MimeType \'%s\' not in map - defaulting to PDF", mimeType));
         return "PDF";
+    }
+
+    /**
+     * Returns mime type based on extension.
+     *
+     * @param extension the extension to map
+     * @return the mapped mime type, or "application/pdf" as default if no mapping was found
+     */
+    public static String getMimetype(String extension) {
+        String ext = extension.toUpperCase();
+        if (mimeTypeMap.containsValue(ext)) {
+            return mimeTypeMap.inverse().get(ext);
+        }
+
+        return MediaType.APPLICATION_PDF_VALUE;
     }
 }
