4ac83c47f5b7daa0e6e19b74ded76adc51723754
mwa <martin.wam@difi.no>
2018-03-13 16:17:38 +0100

    MOVE-654: Added support for querydsl.

--- integrasjonspunkt/src/main/java/no/difi/meldingsutveksling/receipt/ConversationRepository.java
+++ integrasjonspunkt/src/main/java/no/difi/meldingsutveksling/receipt/ConversationRepository.java
@@ -2,20 +2,21 @@ package no.difi.meldingsutveksling.receipt;
 
 import no.difi.meldingsutveksling.nextmove.ConversationDirection;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Profile("!test")
-public interface ConversationRepository  extends CrudRepository<Conversation, String> {
+public interface ConversationRepository  extends CrudRepository<Conversation, String>, QueryDslPredicateExecutor<Conversation> {
+
     Optional<Conversation> findByConvIdAndDirection(Integer convId, ConversationDirection direction);
     List<Conversation> findByConversationIdAndDirection(String conversationId, ConversationDirection direction);
     List<Conversation> findByConversationId(String conversationId);
     List<Conversation> findByPollable(boolean pollable);
-    List<Conversation> findByFinishedAndDirection(boolean finished, ConversationDirection direction);
-    List<Conversation> findByFinishedAndReceiverIdentifierAndDirection(boolean finished, String receiverIdentifier, ConversationDirection direction);
     List<Conversation> findByReceiverIdentifierAndDirection(String receiverIdentifier, ConversationDirection direction);
     List<Conversation> findByDirection(ConversationDirection direction);
     Long countByPollable(boolean pollable);
+
 }
