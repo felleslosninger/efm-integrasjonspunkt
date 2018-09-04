9273d2087cf4467c5313e78269d8ad24593f2b91
Livar Bergheim <lbe@difi.no>
2018-09-04 12:44:55 +0200

    MOVE-860: Retta problem med indexOutOfBounds mot FIKS. Oppgraderte versjon av Spock og la til byte-buddy (Spock-krav for mocking av klasser).

--- integrasjonspunkt/src/main/java/no/difi/meldingsutveksling/receipt/strategy/FiksStatusStrategy.java
+++ integrasjonspunkt/src/main/java/no/difi/meldingsutveksling/receipt/strategy/FiksStatusStrategy.java
@@ -8,6 +8,7 @@ import no.difi.meldingsutveksling.receipt.*;
 import static java.util.Arrays.asList;
 import static no.difi.meldingsutveksling.ks.receipt.DpfReceiptStatus.*;
 import static no.difi.meldingsutveksling.receipt.ConversationMarker.markerFrom;
+import static no.difi.meldingsutveksling.receipt.GenericReceiptStatus.FEIL;
 
 @Slf4j
 public class FiksStatusStrategy implements StatusStrategy {
@@ -26,7 +27,7 @@ public class FiksStatusStrategy implements StatusStrategy {
         if (LEST.toString().equals(messageStatus.getStatus())) {
             conversationService.markFinished(c);
         }
-        if (asList(IKKE_LEVERT.toString(), AVVIST.toString(), MANULT_HANDTERT.toString()).contains(messageStatus.getStatus())) {
+        if (asList(FEIL.toString(), IKKE_LEVERT.toString(), AVVIST.toString(), MANULT_HANDTERT.toString()).contains(messageStatus.getStatus())) {
             conversationService.markFinished(c);
             log.error(markerFrom(c), "DPF conversation {} finished with status {}", c.getConversationId(), messageStatus.getStatus());
         }
--- integrasjonspunkt/src/test/java/no/difi/meldingsutveksling/receipt/strategy/FiksStatusStrategyTest.groovy
+++ integrasjonspunkt/src/test/java/no/difi/meldingsutveksling/receipt/strategy/FiksStatusStrategyTest.groovy
@@ -1,40 +1,43 @@
 package no.difi.meldingsutveksling.receipt.strategy
 
+import no.difi.meldingsutveksling.ServiceIdentifier
 import no.difi.meldingsutveksling.ks.receipt.DpfReceiptStatus
 import no.difi.meldingsutveksling.ks.svarut.SvarUtService
+import no.difi.meldingsutveksling.nextmove.ConversationDirection
 import no.difi.meldingsutveksling.receipt.Conversation
 import no.difi.meldingsutveksling.receipt.ConversationService
+import no.difi.meldingsutveksling.receipt.GenericReceiptStatus
 import no.difi.meldingsutveksling.receipt.MessageStatus
-import org.junit.Before
-import org.junit.Ignore
-import org.junit.Test
-import org.junit.runner.RunWith
-import org.junit.runners.JUnit4
+import spock.lang.*
+import sun.plugin2.message.Message
 
-import static org.mockito.Matchers.any
-import static org.mockito.Mockito.*
+class FiksStatusStrategyTest extends Specification {
 
-@RunWith(JUnit4)
-class FiksStatusStrategyTest {
-    private SvarUtService service
+    @Unroll
+    "given message receipt with status #status, conversation should be set non pollable"() {
+        given:
+        SvarUtService svarUtService = Mock(SvarUtService)
+        ConversationService conversationService = Mock(ConversationService)
+        Conversation conversation = Mock(Conversation)
 
-    @Before
-    void setup() {
-        service = mock(SvarUtService)
-
-    }
-
-    @Ignore
-    @Test
-    void "given message receipt with status read then conversation should be set non pollable"() {
-        def messageReceipt = MessageStatus.of(DpfReceiptStatus.LEST)
-        Conversation conversation = mock(Conversation)
-        def conversationService = mock(ConversationService)
-        FiksStatusStrategy strategy = new FiksStatusStrategy(service, conversationService)
-        when(service.getMessageReceipt(any(Conversation))).thenReturn(messageReceipt)
+        when:
+        def messageReceipt = MessageStatus.of(status)
+        FiksStatusStrategy strategy = new FiksStatusStrategy(svarUtService, conversationService)
+        svarUtService.getMessageReceipt(_) >> messageReceipt
+        conversationService.registerStatus(_ as Conversation, _ as MessageStatus) >> conversation
+        conversation.getReceiverIdentifier() >> "123456785"
+        conversation.getServiceIdentifier() >> ServiceIdentifier.DPF
+        conversation.getDirection() >> ConversationDirection.OUTGOING
+        conversation.getConversationId() >> UUID.randomUUID().toString()
 
         strategy.checkStatus(conversation)
 
-        verify(conversationService).markFinished(conversation)
+        then:
+        1 * conversationService.markFinished(_)
+
+        where:
+        status << [DpfReceiptStatus.LEST, DpfReceiptStatus.IKKE_LEVERT, DpfReceiptStatus.AVVIST, DpfReceiptStatus.MANULT_HANDTERT, GenericReceiptStatus.FEIL]
     }
+
+
 }
