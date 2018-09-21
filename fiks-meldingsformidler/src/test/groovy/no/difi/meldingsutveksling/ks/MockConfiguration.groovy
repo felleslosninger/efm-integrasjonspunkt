813b7d7482e446a150aae681fec59667d28c7635
mwa <martin.wam@difi.no>
2018-09-21 15:59:32 +0200

    MOVE-874: test fix

--- fiks-meldingsformidler/src/test/groovy/no/difi/meldingsutveksling/ks/MockConfiguration.groovy
+++ fiks-meldingsformidler/src/test/groovy/no/difi/meldingsutveksling/ks/MockConfiguration.groovy
@@ -1,10 +1,13 @@
 package no.difi.meldingsutveksling.ks
 
+import com.google.common.collect.Maps
 import no.difi.meldingsutveksling.noarkexchange.NoarkClient
 import no.difi.meldingsutveksling.serviceregistry.ServiceRegistryLookup
 import no.difi.meldingsutveksling.serviceregistry.externalmodel.EntityType
 import no.difi.meldingsutveksling.serviceregistry.externalmodel.InfoRecord
 import no.difi.meldingsutveksling.serviceregistry.externalmodel.ServiceRecord
+import no.difi.meldingsutveksling.serviceregistry.externalmodel.ServiceRecordWrapper
+import org.assertj.core.util.Lists
 import org.mockito.Mockito
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -20,6 +23,7 @@ public class MockConfiguration {
         def lookup = mock(ServiceRegistryLookup)
         def pem = this.getClass().getClassLoader().getResource("difi-cert-test.pem").text
         when(lookup.getServiceRecord(Mockito.any(String), Mockito.eq(DPF))).thenReturn(Optional.of(new ServiceRecord(DPF, "123456789", pem, "http://localhost")))
+        when(lookup.getServiceRecord(Mockito.any(String))).thenReturn(ServiceRecordWrapper.of(new ServiceRecord(DPF, "123456789", pem, "http://localhost"), Lists.newArrayList(), Maps.newHashMap()))
         def infoRecord = new InfoRecord("123456789", "foo", new EntityType("Organisasjonsledd", "ORGL"))
         when(lookup.getInfoRecord(Mockito.any(String))).thenReturn(infoRecord)
         return lookup
