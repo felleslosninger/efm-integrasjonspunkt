613879f8b29e3dfbc94ef15ca63cd527ec898726
jaflaten <ja.flaten91@gmail.com>
2019-03-29 13:24:08 +0100

    Added time to live validation for NextMove messages

--- integrasjonspunkt/src/main/java/no/difi/meldingsutveksling/validation/ReceiverAcceptableDocumentIdentifierValidator.java
+++ integrasjonspunkt/src/main/java/no/difi/meldingsutveksling/validation/ReceiverAcceptableDocumentIdentifierValidator.java
@@ -1,10 +1,9 @@
 package no.difi.meldingsutveksling.validation;
 
 import lombok.extern.slf4j.Slf4j;
-import no.difi.meldingsutveksling.domain.sbdh.DocumentIdentification;
-import no.difi.meldingsutveksling.domain.sbdh.Partner;
-import no.difi.meldingsutveksling.domain.sbdh.StandardBusinessDocumentHeader;
+import no.difi.meldingsutveksling.domain.sbdh.*;
 import no.difi.meldingsutveksling.elma.DocumentIdentifierLookup;
+import no.difi.meldingsutveksling.nextmove.NextMoveRuntimeException;
 import no.difi.vefa.peppol.common.model.DocumentTypeIdentifier;
 import no.difi.vefa.peppol.common.model.ParticipantIdentifier;
 import no.difi.vefa.peppol.common.model.Scheme;
@@ -12,7 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
+import java.time.ZonedDateTime;
 import java.util.List;
+import java.util.Set;
 
 @Slf4j
 public class ReceiverAcceptableDocumentIdentifierValidator implements ConstraintValidator<ReceiverAcceptableDocumentIdentifier, StandardBusinessDocumentHeader> {
@@ -37,6 +38,11 @@ public class ReceiverAcceptableDocumentIdentifierValidator implements Constraint
             return true;
         }
 
+        if (!getExpectedResponseDateTime(header)) {
+            return false;
+        }
+
+   // header.getBusinessscope()
         DocumentTypeIdentifier standard = getStandard(header);
 
         List<DocumentTypeIdentifier> acceptedDocumentIdentifiers = documentIdentifierLookup.getDocumentIdentifiers(participantIdentifier);
@@ -71,4 +77,28 @@ public class ReceiverAcceptableDocumentIdentifierValidator implements Constraint
 
         return DocumentTypeIdentifier.of(documentIdentification.getStandard());
     }
+
+    public Boolean getExpectedResponseDateTime(StandardBusinessDocumentHeader header) {
+        Set<Scope> scope = header.getBusinessScope().getScope();
+        ZonedDateTime now = ZonedDateTime.now();
+        ZonedDateTime zonedDateTime;
+        zonedDateTime = gettingExpectedResponseDateTime(scope, now);
+
+        if (zonedDateTime.isAfter(now) || now.equals(zonedDateTime)) {
+            return true;
+        }
+        else {
+            log.error("ExpectedResponseDateTime is expired. Message will not be handled further. Please resend...");
+            return false;}
+    }
+
+    public ZonedDateTime gettingExpectedResponseDateTime(Set<Scope> scope, ZonedDateTime now) {
+        ZonedDateTime zonedDateTime = scope.stream().flatMap(s-> s.getScopeInformation()
+                .stream())
+                .map(CorrelationInformation::getExpectedResponseDateTime)
+                .findFirst()
+                .orElseThrow(() -> new NextMoveRuntimeException("No ExpectedResponseDateTime found"));
+        return zonedDateTime;
+    }
+
 }
