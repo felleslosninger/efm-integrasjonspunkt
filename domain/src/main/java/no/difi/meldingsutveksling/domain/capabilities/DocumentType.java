4a9058b287140e0a910601bb6732c831ab434f0d
Frode Bjerkholt <46349741+FrodeBjerkholt@users.noreply.github.com>
2019-05-09 12:02:02 +0200

    Extending the Capabilities lookup

---
+++ domain/src/main/java/no/difi/meldingsutveksling/domain/capabilities/DocumentType.java
@@ -0,0 +1,10 @@
+package no.difi.meldingsutveksling.domain.capabilities;
+
+import lombok.Data;
+
+@Data
+public class DocumentType {
+
+    private String type;
+    private String standard;
+}
---
+++ integrasjonspunkt/src/test/java/no/difi/meldingsutveksling/cucumber/CapabilitySteps.java
@@ -0,0 +1,49 @@
+package no.difi.meldingsutveksling.cucumber;
+
+import cucumber.api.java.After;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import lombok.RequiredArgsConstructor;
+import org.json.JSONException;
+import org.junit.Before;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RequiredArgsConstructor
+public class CapabilitySteps {
+
+    private final TestRestTemplate testRestTemplate;
+    private ResponseEntity<String> response;
+
+    @Before
+    public void before() {
+
+    }
+
+    @After
+    public void after() {
+        this.response = null;
+    }
+
+    @Given("^I request all capabilities for \"([^\"]*)\"$")
+    public void iRequestAllCapabilitiesFor(String receiverId) {
+        this.response = testRestTemplate.exchange(
+                "/api/capabilities/{receiverId}",
+                HttpMethod.GET,
+                null,
+                String.class,
+                receiverId);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+
+    @Then("^the returned capabilties are:$")
+    public void theReturnedCapabiltiesAre(String expectedJson) throws JSONException {
+        JSONAssert.assertEquals(expectedJson, response.getBody(), true);
+    }
+}
