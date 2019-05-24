22cfb172ce47f0a1414e19ebd260277a4bf396c4
jaflaten <ja.flaten91@gmail.com>
2019-05-24 13:33:30 +0200

    TimeToLiveTask sets status and marks conversation as finished every 5 seconds on expired messages

---
+++ integrasjonspunkt/src/main/java/no/difi/meldingsutveksling/nextmove/v2/TimeToLiveTask.java
@@ -0,0 +1,26 @@
+package no.difi.meldingsutveksling.nextmove.v2;
+
+import lombok.RequiredArgsConstructor;
+import no.difi.meldingsutveksling.receipt.*;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class TimeToLiveTask {
+    private final ConversationService conversationService;
+    private final ConversationRepository repo;
+    private final MessageStatusFactory messageStatusFactory;
+
+    @Scheduled(fixedDelay = 5000)
+    public void checkStatus() {
+        List<Conversation> conversations = repo.findByExpiryLessThanEqualAndFinished(ZonedDateTime.now(), false);
+        for (Conversation c : conversations) {
+            conversationService.registerStatus(c, messageStatusFactory.getMessageStatus(ReceiptStatus.LEVETID_UTLOPT));
+            conversationService.markFinished(c);
+        }
+    }
+}
