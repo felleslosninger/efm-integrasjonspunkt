fe2e8eabd6983a6a0c5f3e400aa41be74cd08459
Frode Bjerkholt <46349741+FrodeBjerkholt@users.noreply.github.com>
2019-08-30 10:01:16 +0200

    Adding handling of sakStatus R, X and KU in SakstatusMapper

--- domain/src/main/java/no/difi/meldingsutveksling/domain/arkivmelding/SaksstatusMapper.java
+++ domain/src/main/java/no/difi/meldingsutveksling/domain/arkivmelding/SaksstatusMapper.java
@@ -3,34 +3,44 @@ package no.difi.meldingsutveksling.domain.arkivmelding;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 import no.arkivverket.standarder.noark5.metadatakatalog.Saksstatus;
-import no.difi.meldingsutveksling.HashBiMap;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @UtilityClass
 public class SaksstatusMapper {
-    private static final HashBiMap<String, Saksstatus> mapper = new HashBiMap<>();
+    private static final Map<String, Saksstatus> string2Status = new HashMap<>();
+    private static final Map<Saksstatus, String> status2string = new HashMap<>();
 
     static {
-        mapper.put("A", Saksstatus.AVSLUTTET);
-        mapper.put("B", Saksstatus.UNDER_BEHANDLING);
-        mapper.put("U", Saksstatus.UTGÅR);
+        string2Status.put("A", Saksstatus.AVSLUTTET);
+        string2Status.put("B", Saksstatus.UNDER_BEHANDLING);
+        string2Status.put("U", Saksstatus.UTGÅR);
+        string2Status.put("R", Saksstatus.UNDER_BEHANDLING); // Reservert sak. (Midlertidig = 1, Lukket = 0)
+        string2Status.put("X", Saksstatus.UNDER_BEHANDLING); //  Saken er ikke gjenstand for oppfølging, tilsvarer blankt felt Sak avsluttet i Noark-3. (Midlertidig = 0, Lukket = 0)
+        string2Status.put("KU", Saksstatus.AVSLUTTET); // Kopiert utdrag. Skal kun benyttes ved eksport av et utdrag av en sak og da kun i den eksporterte versjonen av saken. Ved import av  en sak med status KU skal den håndteres på linje med status A, dvs saken skal ikke kunne endres.
+
+        status2string.put(Saksstatus.AVSLUTTET, "A");
+        status2string.put(Saksstatus.UNDER_BEHANDLING, "B");
+        status2string.put(Saksstatus.UTGÅR, "U");
     }
 
     public static String getNoarkType(Saksstatus status) {
-        if (!mapper.containsValue(status)) {
+        if (!string2Status.containsValue(status)) {
             log.error("Saksstatus \"{}\" not registered in map, defaulting to \"{}\"", status.value(), Saksstatus.UNDER_BEHANDLING.value());
-            return mapper.inverse().get(Saksstatus.UNDER_BEHANDLING);
+            return status2string.get(Saksstatus.UNDER_BEHANDLING);
         }
 
-        return mapper.inverse().get(status);
+        return status2string.get(status);
     }
 
     static Saksstatus getArkivmeldingType(String status) {
-        if (!mapper.containsKey(status)) {
-            log.error("Saksstatus \"{}\" not registered in map, defaulting to \"{}\"", status, mapper.inverse().get(Saksstatus.UNDER_BEHANDLING));
+        if (!string2Status.containsKey(status)) {
+            log.error("Saksstatus \"{}\" not registered in map, defaulting to \"{}\"", status, status2string.get(Saksstatus.UNDER_BEHANDLING));
             return Saksstatus.UNDER_BEHANDLING;
         }
 
-        return mapper.get(status);
+        return string2Status.get(status);
     }
 }
