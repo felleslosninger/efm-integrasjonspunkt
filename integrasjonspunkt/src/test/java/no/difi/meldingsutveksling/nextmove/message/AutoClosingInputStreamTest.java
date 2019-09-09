391a4bd0e1f7d734c456ac82672e36e37df77136
Frode Bjerkholt <46349741+FrodeBjerkholt@users.noreply.github.com>
2019-09-09 15:44:31 +0200

    Adding test for callback

--- integrasjonspunkt/src/test/java/no/difi/meldingsutveksling/nextmove/message/AutoClosingInputStreamTest.java
+++ integrasjonspunkt/src/test/java/no/difi/meldingsutveksling/nextmove/message/AutoClosingInputStreamTest.java
@@ -11,6 +11,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class AutoClosingInputStreamTest {
 
+    private boolean callbackCalled;
+
     @Test
     public void testRead() throws IOException {
         AutoClosingInputStream is = new AutoClosingInputStream(new ByteArrayInputStream("Yo".getBytes(StandardCharsets.UTF_8)), () -> {
@@ -48,5 +50,19 @@ public class AutoClosingInputStreamTest {
         assertThat(is.read(buffer, 1, 3)).isEqualTo(2);
         assertThat(is.isClosed()).isTrue();
         assertThat(is.read(buffer, 1, 3)).isEqualTo(-1);
+    }
+
+    @Test
+    public void testCallback() throws IOException {
+        callbackCalled = false;
+
+        AutoClosingInputStream is = new AutoClosingInputStream(new ByteArrayInputStream("Hello".getBytes(StandardCharsets.UTF_8)), () -> {
+            callbackCalled = true;
+        });
+
+        assertThat(callbackCalled).isFalse();
+        byte[] buffer = new byte[5];
+        assertThat(is.read(buffer)).isEqualTo(5);
+        assertThat(callbackCalled).isTrue();
     }
 }
