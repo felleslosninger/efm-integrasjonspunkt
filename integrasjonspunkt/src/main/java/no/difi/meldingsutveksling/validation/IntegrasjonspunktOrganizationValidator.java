a6a377ea8de1801e2f118bbc01b98b4cf4d32887
Martin Wam <martin.wam@gmail.com>
2020-01-15 11:24:21 +0100

    MOVE-1581: DPV onBehalfOf

--- integrasjonspunkt/src/main/java/no/difi/meldingsutveksling/validation/IntegrasjonspunktOrganizationValidator.java
+++ integrasjonspunkt/src/main/java/no/difi/meldingsutveksling/validation/IntegrasjonspunktOrganizationValidator.java
@@ -2,7 +2,6 @@ package no.difi.meldingsutveksling.validation;
 
 import no.difi.meldingsutveksling.config.IntegrasjonspunktProperties;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
@@ -23,6 +22,10 @@ public class IntegrasjonspunktOrganizationValidator implements ConstraintValidat
     public boolean isValid(String value, ConstraintValidatorContext context) {
         if (value == null) {
             return true;
+        }
+        String[] split = value.split(":");
+        if (split.length == 3) {
+            return (split[0]+":"+split[1]).equals(expectedValue);
         }
 
         return Objects.equals(expectedValue, value);
