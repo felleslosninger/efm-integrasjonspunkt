59a3f62d081331ac45a09cdd879b27556c3b50d5
Martin Wam <martin.wam@gmail.com>
2020-01-30 12:18:04 +0100

    MOVE-1726: Error sending AppReceipt should not result in message retry

--- integrasjonspunkt/src/main/java/no/difi/meldingsutveksling/noarkexchange/receive/BestEduAppReceiptService.java
+++ integrasjonspunkt/src/main/java/no/difi/meldingsutveksling/noarkexchange/receive/BestEduAppReceiptService.java
@@ -1,5 +1,6 @@
 package no.difi.meldingsutveksling.noarkexchange.receive;
 
+import lombok.extern.slf4j.Slf4j;
 import no.difi.meldingsutveksling.DocumentType;
 import no.difi.meldingsutveksling.UUIDGenerator;
 import no.difi.meldingsutveksling.config.IntegrasjonspunktProperties;
@@ -7,13 +8,12 @@ import no.difi.meldingsutveksling.core.BestEduConverter;
 import no.difi.meldingsutveksling.dokumentpakking.service.SBDFactory;
 import no.difi.meldingsutveksling.domain.Organisasjonsnummer;
 import no.difi.meldingsutveksling.domain.sbdh.StandardBusinessDocument;
+import no.difi.meldingsutveksling.logging.NextMoveMessageMarkers;
 import no.difi.meldingsutveksling.nextmove.ArkivmeldingKvitteringMessage;
 import no.difi.meldingsutveksling.nextmove.KvitteringStatusMessage;
 import no.difi.meldingsutveksling.nextmove.NextMoveOutMessage;
 import no.difi.meldingsutveksling.nextmove.v2.NextMoveMessageService;
-import no.difi.meldingsutveksling.noarkexchange.AppReceiptFactory;
-import no.difi.meldingsutveksling.noarkexchange.NoarkClient;
-import no.difi.meldingsutveksling.noarkexchange.PutMessageRequestFactory;
+import no.difi.meldingsutveksling.noarkexchange.*;
 import no.difi.meldingsutveksling.noarkexchange.schema.AppReceiptType;
 import no.difi.meldingsutveksling.noarkexchange.schema.PutMessageRequestType;
 import org.springframework.beans.factory.ObjectProvider;
@@ -21,8 +21,11 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
+import static no.difi.meldingsutveksling.logging.NextMoveMessageMarkers.markerFrom;
+
 @Service
-public class BestEduErrorAppReceiptService {
+@Slf4j
+public class BestEduAppReceiptService {
 
     private final IntegrasjonspunktProperties properties;
     private final SBDFactory createSBD;
@@ -30,20 +33,23 @@ public class BestEduErrorAppReceiptService {
     private final PutMessageRequestFactory putMessageRequestFactory;
     private final NoarkClient noarkClient;
     private final UUIDGenerator uuidGenerator;
+    private final ConversationIdEntityRepo conversationIdEntityRepo;
 
-    public BestEduErrorAppReceiptService(
+    public BestEduAppReceiptService(
             IntegrasjonspunktProperties properties,
             SBDFactory createSBD,
             @Lazy NextMoveMessageService nextMoveMessageService,
             PutMessageRequestFactory putMessageRequestFactory,
-            @Qualifier("localNoark") ObjectProvider<NoarkClient> noarkClient,
-            UUIDGenerator uuidGenerator) {
+            @Qualifier("localNoark") ObjectProvider<NoarkClient> localNoark,
+            UUIDGenerator uuidGenerator,
+            ConversationIdEntityRepo conversationIdEntityRepo) {
         this.properties = properties;
         this.createSBD = createSBD;
         this.nextMoveMessageService = nextMoveMessageService;
         this.putMessageRequestFactory = putMessageRequestFactory;
-        this.noarkClient = noarkClient.getIfAvailable();
+        this.noarkClient = localNoark.getIfAvailable();
         this.uuidGenerator = uuidGenerator;
+        this.conversationIdEntityRepo = conversationIdEntityRepo;
     }
 
     void sendBestEduErrorAppReceipt(NextMoveOutMessage message, String errorText) {
@@ -68,5 +74,29 @@ public class BestEduErrorAppReceiptService {
 
         NextMoveOutMessage message = nextMoveMessageService.createMessage(receiptSbd);
         nextMoveMessageService.sendMessage(message);
+    }
+
+    /**
+     * Used to send AppReceipt to sender for other cases than DPO.
+     *
+     * @param message nextmove message
+     */
+    public void sendAppReceiptToLocalNoark(NextMoveOutMessage message) {
+        String conversationId = message.getConversationId();
+        ConversationIdEntity convId = conversationIdEntityRepo.findByNewConversationId(message.getConversationId());
+        if (convId != null) {
+            log.warn("Found {} which maps to conversation {} with invalid UUID - overriding in AppReceipt.", message.getConversationId(), convId.getOldConversationId());
+            conversationId = convId.getOldConversationId();
+            conversationIdEntityRepo.delete(convId);
+        }
+        AppReceiptType appReceipt = AppReceiptFactory.from("OK", "None", "OK");
+        PutMessageRequestType putMessage = putMessageRequestFactory.createAndSwitchSenderReceiver(message.getSbd(),
+                BestEduConverter.appReceiptAsString(appReceipt),
+                conversationId);
+        try {
+            noarkClient.sendEduMelding(putMessage);
+        } catch (Exception e) {
+            log.error(markerFrom(message), "Error sending AppReceipt to localNoark", e);
+        }
     }
 }
