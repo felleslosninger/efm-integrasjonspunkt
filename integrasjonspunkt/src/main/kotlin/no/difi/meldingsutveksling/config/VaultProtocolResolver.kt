5c2f934a302949b1a5d37bae075110cb6e74a833
Martin Wam <martin.wam@gmail.com>
2020-04-27 10:50:09 +0200

    MOVE-1642: load resources from vault

---
+++ integrasjonspunkt/src/main/kotlin/no/difi/meldingsutveksling/config/VaultProtocolResolver.kt
@@ -0,0 +1,50 @@
+package no.difi.meldingsutveksling.config
+
+import no.difi.meldingsutveksling.util.logger
+import org.springframework.context.ApplicationContextInitializer
+import org.springframework.context.ConfigurableApplicationContext
+import org.springframework.core.Ordered
+import org.springframework.core.env.Environment
+import org.springframework.core.io.ByteArrayResource
+import org.springframework.vault.authentication.TokenAuthentication
+import org.springframework.vault.client.VaultEndpoint
+import org.springframework.vault.core.VaultTemplate
+import org.springframework.vault.core.util.KeyValueDelegate
+import java.lang.RuntimeException
+import java.net.URI
+import java.util.*
+
+class VaultProtocolResolver : ApplicationContextInitializer<ConfigurableApplicationContext>, Ordered {
+
+    val log = logger()
+    private val vaultResources = mutableMapOf<String, ByteArrayResource>()
+
+    override fun initialize(c: ConfigurableApplicationContext) {
+        val prefix = "vault:"
+        c.addProtocolResolver { location, _ ->
+            if (location.startsWith(prefix)) {
+                if (vaultResources.isEmpty()) {
+                    val props = c.getBean(Environment::class.java)
+                    val vt = VaultTemplate(VaultEndpoint.from(URI.create(props.getProperty("vault.uri", ""))),
+                            TokenAuthentication(props.getProperty("vault.token", "")))
+                    val del = KeyValueDelegate(vt)
+                    val path = props.getProperty("vault.resource-path", "")
+                    del.isVersioned(path)
+                    del.getSecret(path)?.data?.entries?.map {
+                        val decode = Base64.getDecoder().decode((it.value as String).replace("\n", ""))
+                        vaultResources[it.key] = ByteArrayResource(decode)
+                    }
+                }
+                vaultResources[location.removePrefix(prefix)]
+                        ?: throw RuntimeException("Vault resource \"$location\" defined, but not located in vault")
+            } else {
+                null
+            }
+        }
+    }
+
+    override fun getOrder(): Int {
+        return 1
+    }
+
+}
