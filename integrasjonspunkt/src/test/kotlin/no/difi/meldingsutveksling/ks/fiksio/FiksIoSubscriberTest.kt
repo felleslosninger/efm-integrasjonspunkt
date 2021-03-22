195c938615d2841f217f1dc7df6158543dcd9250
Martin Wam <martin.wam@gmail.com>
2021-03-22 16:00:54 +0100

    MOVE-2359: cleaned up test

--- integrasjonspunkt/src/test/kotlin/no/difi/meldingsutveksling/ks/fiksio/FiksIoSubscriberTest.kt
+++ integrasjonspunkt/src/test/kotlin/no/difi/meldingsutveksling/ks/fiksio/FiksIoSubscriberTest.kt
@@ -2,7 +2,6 @@ package no.difi.meldingsutveksling.ks.fiksio
 
 import io.mockk.*
 import io.mockk.impl.annotations.MockK
-import no.difi.meldingsutveksling.NextMoveConsts.SBD_FILE
 import no.difi.meldingsutveksling.ServiceIdentifier
 import no.difi.meldingsutveksling.api.NextMoveQueue
 import no.difi.meldingsutveksling.config.IntegrasjonspunktProperties
@@ -14,12 +13,8 @@ import no.ks.fiks.io.client.model.MeldingId
 import no.ks.fiks.io.client.model.MottattMelding
 import org.junit.Before
 import org.junit.Test
-import java.io.ByteArrayOutputStream
 import java.io.InputStream
 import java.util.*
-import java.util.zip.ZipEntry
-import java.util.zip.ZipInputStream
-import java.util.zip.ZipOutputStream
 
 class FiksIoSubscriberTest {
 
@@ -32,24 +27,12 @@ class FiksIoSubscriberTest {
     @MockK
     lateinit var props: IntegrasjonspunktProperties
 
-    lateinit var zipInputStream: ZipInputStream
     lateinit var sbd: StandardBusinessDocument
     lateinit var fiksIoSubscriber: FiksIoSubscriber
 
     @Before
     fun before() {
         MockKAnnotations.init(this)
-
-        val sbdStream = this.javaClass.classLoader.getResourceAsStream("sbd/StandardBusinessDocument.json")
-                ?: throw RuntimeException("sbd test file not found")
-        val bos = ByteArrayOutputStream()
-        val zos = ZipOutputStream(bos)
-        zos.putNextEntry(ZipEntry(SBD_FILE))
-        val sbdBytes = sbdStream.readBytes()
-        zos.write(sbdBytes)
-        zos.closeEntry()
-        zos.close()
-        zipInputStream = ZipInputStream(bos.toByteArray().inputStream())
 
         every { fiksIOKlient.newSubscription(any()) } just Runs
         every { props.org.number } returns "123123123"
@@ -65,11 +48,8 @@ class FiksIoSubscriberTest {
         val mottattMelding = mockkClass(MottattMelding::class) {
             every { meldingId } returns MeldingId(UUID.fromString("6d16a689-da59-4d22-8e3e-82bcb9169ccb"))
             every { meldingType } returns "no.digdir.einnsyn.v1"
-
-            every { dekryptertZipStream } returns zipInputStream
             every { kryptertStream } returns mockkClass(InputStream::class)
         }
-
         val svarSender = mockkClass(SvarSender::class, relaxed = true)
 
         val m = fiksIoSubscriber.javaClass.getDeclaredMethod("handleMessage", MottattMelding::class.java, SvarSender::class.java)
