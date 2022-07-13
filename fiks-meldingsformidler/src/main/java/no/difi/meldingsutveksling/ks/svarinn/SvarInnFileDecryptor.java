27bcdf21dc7b6ce26ba90cc726a0c65bcfea22a8
Johannes Molland <12180816+johannmo@users.noreply.github.com>
2022-07-13 12:58:37 +0200

    fix: MOVE-2894: Use FIKS keystore for decrypting
    - Use difi.move.fiks.keystore for decrypting SvarInn messages.

--- fiks-meldingsformidler/src/main/java/no/difi/meldingsutveksling/ks/svarinn/SvarInnFileDecryptor.java
+++ fiks-meldingsformidler/src/main/java/no/difi/meldingsutveksling/ks/svarinn/SvarInnFileDecryptor.java
@@ -1,17 +1,21 @@
 package no.difi.meldingsutveksling.ks.svarinn;
 
-import lombok.RequiredArgsConstructor;
+import no.difi.meldingsutveksling.config.IntegrasjonspunktProperties;
 import no.difi.meldingsutveksling.dokumentpakking.service.DecryptCMSDocument;
 import no.difi.move.common.cert.KeystoreHelper;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class SvarInnFileDecryptor {
 
     private final DecryptCMSDocument decryptCMSDocument;
     private final KeystoreHelper keystoreHelper;
+
+    public SvarInnFileDecryptor(DecryptCMSDocument decryptCMSDocument, IntegrasjonspunktProperties properties) {
+        this.decryptCMSDocument = decryptCMSDocument;
+        this.keystoreHelper = new KeystoreHelper(properties.getFiks().getKeystore());
+    }
 
     public Resource decrypt(Resource encrypted) {
         return decryptCMSDocument.decrypt(DecryptCMSDocument.Input.builder()
