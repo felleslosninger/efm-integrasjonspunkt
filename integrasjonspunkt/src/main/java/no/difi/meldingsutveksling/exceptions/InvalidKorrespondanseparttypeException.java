990e1f299df576460e02c6aad1de5a70f3d481dc
joachimhagheim <47362824+joachimhagheim@users.noreply.github.com>
2022-10-31 14:15:28 +0100

    MOVE-2974 Added message for InvalidKorrespondanseparttypeException  in messages.properties, and removed import of wrong HttpStatusCodeException

--- integrasjonspunkt/src/main/java/no/difi/meldingsutveksling/exceptions/InvalidKorrespondanseparttypeException.java
+++ integrasjonspunkt/src/main/java/no/difi/meldingsutveksling/exceptions/InvalidKorrespondanseparttypeException.java
@@ -1,7 +1,6 @@
 package no.difi.meldingsutveksling.exceptions;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.client.HttpStatusCodeException;
 
 public class InvalidKorrespondanseparttypeException extends HttpStatusCodeException {
 
