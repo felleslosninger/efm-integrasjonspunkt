cdbc2fd3d3829d4afcc7961b265aef5f02c1d5ab
joachimhagheim <47362824+joachimhagheim@users.noreply.github.com>
2023-05-04 15:02:46 +0200

    MOVE-3242_Finally fix double MOTTATT status, might break proxy client?

--- integrasjonspunkt/src/main/java/no/difi/meldingsutveksling/DpiReceiptHandler.java
+++ integrasjonspunkt/src/main/java/no/difi/meldingsutveksling/DpiReceiptHandler.java
@@ -32,6 +32,8 @@ public class DpiReceiptHandler {
         if (conversation.isPresent()) {
             MessageStatus status = externalReceipt.toMessageStatus();
             boolean isProxyClient = "xmlsoap".equals(properties.getDpi().getReceiptType());
+
+            // If LEVERT comes before MOTTATT
             if (ReceiptStatus.valueOf(status.getStatus()) == LEVERT && !isProxyClient && !conversation.get().getMessageStatuses().stream().anyMatch(ms -> ms.getStatus().equals("MOTTATT"))) {
                 // Ensures that status MOTTATT is registered before LEVERT
                 // If MOTTATT was previously registered this attempt will be discarded and not cause duplicates
@@ -41,8 +43,18 @@ public class DpiReceiptHandler {
                 conversationService.registerStatus(conversation.get(), mottatt);
             }
 
-            conversationService.registerStatus(conversation.get(), status);
-            log.debug(externalReceipt.logMarkers(), "Updated receipt (DPI)");
+            // If MOTTATT comes after LEVERT.
+            if (ReceiptStatus.valueOf(status.getStatus()) == MOTTATT) {
+                if (!isProxyClient && !conversation.get().getMessageStatuses().stream().anyMatch(ms -> ms.getStatus().equals("MOTTATT"))) {
+                    conversationService.registerStatus(conversation.get(), status);
+                    log.debug(externalReceipt.logMarkers(), "Updated receipt (DPI)");
+                }
+            } else {
+                conversationService.registerStatus(conversation.get(), status);
+                log.debug(externalReceipt.logMarkers(), "Updated receipt (DPI)");
+            }
+
+
         } else {
             log.warn("Unknown conversationID = {}", conversationId);
         }
