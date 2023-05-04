db501e9e91ab711fbb5bb7445faf684dbf84bd59
joachimhagheim <47362824+joachimhagheim@users.noreply.github.com>
2023-05-04 12:19:05 +0200

    MOVE-3242_Added if  statement and comments that hopefully fixes and explains the problem

--- integrasjonspunkt/src/main/java/no/difi/meldingsutveksling/DpiReceiptHandler.java
+++ integrasjonspunkt/src/main/java/no/difi/meldingsutveksling/DpiReceiptHandler.java
@@ -32,6 +32,8 @@ public class DpiReceiptHandler {
         if (conversation.isPresent()) {
             MessageStatus status = externalReceipt.toMessageStatus();
             boolean isProxyClient = "xmlsoap".equals(properties.getDpi().getReceiptType());
+
+            // If LEVERT comes before MOTTATT
             if (ReceiptStatus.valueOf(status.getStatus()) == LEVERT && !isProxyClient && !conversation.get().getMessageStatuses().stream().anyMatch(ms -> ms.getStatus().equals("MOTTATT"))) {
                 // Ensures that status MOTTATT is registered before LEVERT
                 // If MOTTATT was previously registered this attempt will be discarded and not cause duplicates
@@ -41,8 +43,25 @@ public class DpiReceiptHandler {
                 conversationService.registerStatus(conversation.get(), mottatt);
             }
 
-            conversationService.registerStatus(conversation.get(), status);
-            log.debug(externalReceipt.logMarkers(), "Updated receipt (DPI)");
+            // If MOTTATT comes after LEVERT. While yes, this always happens if the above if-statement is true,
+            // it is a separate incident at a separate time, and therefore probably needs its own if.
+            // I'm assuming that the proxy client actually crashes when you register MOTTATT status no matter what,
+            // as mentioned above, and that it isn't just because it can't handle TWO instances of MOTTATT or something.
+            if (ReceiptStatus.valueOf(status.getStatus()) == MOTTATT) {
+                if (!isProxyClient && !conversation.get().getMessageStatuses().stream().anyMatch(ms -> ms.getStatus().equals("MOTTATT"))) {
+                    conversationService.registerStatus(conversation.get(), status);
+                    log.debug(externalReceipt.logMarkers(), "Updated receipt (DPI)");
+                }
+            }
+            // Only MOTTATT has been a problem, everything else should work.
+            // Actually yeah, why did this work previously if the proxy client crashed when it got MOTTATT?
+            // Did it ever work with the proxy client?
+            else {
+                conversationService.registerStatus(conversation.get(), status);
+                log.debug(externalReceipt.logMarkers(), "Updated receipt (DPI)");
+            }
+
+
         } else {
             log.warn("Unknown conversationID = {}", conversationId);
         }
