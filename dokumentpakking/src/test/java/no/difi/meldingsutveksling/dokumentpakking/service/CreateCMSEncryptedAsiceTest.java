16716d269006ed85ecb3f1ce2dc0ac93df15baf4
Johannes Molland <12180816+johannmo@users.noreply.github.com>
2024-08-02 18:54:57 +0200

    refactor: Remove unnecessary import

--- dokumentpakking/src/test/java/no/difi/meldingsutveksling/dokumentpakking/service/CreateCMSEncryptedAsiceTest.java
+++ dokumentpakking/src/test/java/no/difi/meldingsutveksling/dokumentpakking/service/CreateCMSEncryptedAsiceTest.java
@@ -18,7 +18,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
