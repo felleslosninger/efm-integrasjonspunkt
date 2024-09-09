049588b369413b3e3343150c78ffca6a81f2de7f
Johannes Molland <12180816+johannmo@users.noreply.github.com>
2024-09-09 22:05:24 +0200

    test: Fix import

--- integrasjonspunkt/src/test/java/no/difi/meldingsutveksling/cucumber/SimilarToXmlPattern.java
+++ integrasjonspunkt/src/test/java/no/difi/meldingsutveksling/cucumber/SimilarToXmlPattern.java
@@ -16,7 +16,7 @@
 package no.difi.meldingsutveksling.cucumber;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.github.tomakehurst.wiremock.common.Xml;
+import com.github.tomakehurst.wiremock.common.xml.Xml;
 import com.github.tomakehurst.wiremock.matching.MatchResult;
 import com.github.tomakehurst.wiremock.matching.StringValuePattern;
 import org.springframework.util.StringUtils;
