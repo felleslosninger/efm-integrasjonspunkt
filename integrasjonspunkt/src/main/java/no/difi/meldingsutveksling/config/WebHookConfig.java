27a20a1d8e75e48aab38c6b6696a85e8e8f46588
Thore Johnsen <thore@jpro.no>
2025-01-24 13:41:54 +0100

    MOVE-3657 Fixed failing IntegrasjonspunktErrorController tests

--- integrasjonspunkt/src/main/java/no/difi/meldingsutveksling/config/WebHookConfig.java
+++ integrasjonspunkt/src/main/java/no/difi/meldingsutveksling/config/WebHookConfig.java
@@ -6,7 +6,6 @@ import no.difi.meldingsutveksling.webhooks.UrlPusher;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.web.client.DefaultResponseErrorHandler;
 
