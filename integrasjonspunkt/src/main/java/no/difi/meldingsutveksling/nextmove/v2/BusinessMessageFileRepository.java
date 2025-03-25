963d872f116d8a06c8bdd8d69d5369282c5d598c
Thore Johnsen <thore@jpro.no>
2025-03-25 10:31:15 +0100

    MOVE-3684 Verified BMFR delete and removed comments

--- integrasjonspunkt/src/main/java/no/difi/meldingsutveksling/nextmove/v2/BusinessMessageFileRepository.java
+++ integrasjonspunkt/src/main/java/no/difi/meldingsutveksling/nextmove/v2/BusinessMessageFileRepository.java
@@ -10,7 +10,7 @@ public interface BusinessMessageFileRepository extends JpaRepository<BusinessMes
 
     @Transactional
     @Modifying
-    @Query("DELETE FROM BusinessMessageFile b WHERE b.message.id = ?1") // FIXME tripe check that this actually deletes what it used to do, b.id vs b.message.id
+    @Query("DELETE FROM BusinessMessageFile b WHERE b.message.id = ?1")
     void deleteFilesByMessageId(Long messageId);
 
 }
