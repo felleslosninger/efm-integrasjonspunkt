6795aa98daa894b4a3a4e88e8f2e52eac4a40394
Thore Johnsen <thore@jpro.no>
2025-06-17 14:44:05 +0200

    Text block policy

--- altinn-v3-client/src/test/java/altinn3/ResourceApiTest.java
+++ altinn-v3-client/src/test/java/altinn3/ResourceApiTest.java
@@ -23,107 +23,109 @@ public class ResourceApiTest {
         String accessToken = ApiUtils.retrieveAccessToken("altinn:resourceregistry/resource.write");
         System.out.println(accessToken);
 
-        String xacml = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
-                "<xacml:Policy PolicyId=\"urn:altinn:policyid:1\" Version=\"1.0\" RuleCombiningAlgId=\"urn:oasis:names:tc:xacml:3.0:rule-combining-algorithm:deny-overrides\" xmlns:xacml=\"urn:oasis:names:tc:xacml:3.0:core:schema:wd-17\">\n" +
-                "  <xacml:Target />\n" +
-                "  <xacml:Rule RuleId=\"urn:altinn:resource:eformidling-meldingsteneste-test:ruleid:2\" Effect=\"Permit\">\n" +
-                "    <xacml:Description>Post/arkiv og privatperson skal ha tilgang til å lese og abonnere på meldinger</xacml:Description>\n" +
-                "    <xacml:Target>\n" +
-                "      <xacml:AnyOf>\n" +
-                "        <xacml:AllOf>\n" +
-                "          <xacml:Match MatchId=\"urn:oasis:names:tc:xacml:3.0:function:string-equal-ignore-case\">\n" +
-                "            <xacml:AttributeValue DataType=\"http://www.w3.org/2001/XMLSchema#string\">a0236</xacml:AttributeValue>\n" +
-                "            <xacml:AttributeDesignator AttributeId=\"urn:altinn:rolecode\" Category=\"urn:oasis:names:tc:xacml:1.0:subject-category:access-subject\" DataType=\"http://www.w3.org/2001/XMLSchema#string\" MustBePresent=\"false\" />\n" +
-                "          </xacml:Match>\n" +
-                "        </xacml:AllOf>\n" +
-                "        <xacml:AllOf>\n" +
-                "          <xacml:Match MatchId=\"urn:oasis:names:tc:xacml:3.0:function:string-equal-ignore-case\">\n" +
-                "            <xacml:AttributeValue DataType=\"http://www.w3.org/2001/XMLSchema#string\">priv</xacml:AttributeValue>\n" +
-                "            <xacml:AttributeDesignator AttributeId=\"urn:altinn:rolecode\" Category=\"urn:oasis:names:tc:xacml:1.0:subject-category:access-subject\" DataType=\"http://www.w3.org/2001/XMLSchema#string\" MustBePresent=\"false\" />\n" +
-                "          </xacml:Match>\n" +
-                "        </xacml:AllOf>\n" +
-                "      </xacml:AnyOf>\n" +
-                "      <xacml:AnyOf>\n" +
-                "        <xacml:AllOf>\n" +
-                "          <xacml:Match MatchId=\"urn:oasis:names:tc:xacml:1.0:function:string-equal\">\n" +
-                "            <xacml:AttributeValue DataType=\"http://www.w3.org/2001/XMLSchema#string\">eformidling-meldingsteneste-test</xacml:AttributeValue>\n" +
-                "            <xacml:AttributeDesignator AttributeId=\"urn:altinn:resource\" Category=\"urn:oasis:names:tc:xacml:3.0:attribute-category:resource\" DataType=\"http://www.w3.org/2001/XMLSchema#string\" MustBePresent=\"false\" />\n" +
-                "          </xacml:Match>\n" +
-                "        </xacml:AllOf>\n" +
-                "      </xacml:AnyOf>\n" +
-                "      <xacml:AnyOf>\n" +
-                "        <xacml:AllOf>\n" +
-                "          <xacml:Match MatchId=\"urn:oasis:names:tc:xacml:3.0:function:string-equal-ignore-case\">\n" +
-                "            <xacml:AttributeValue DataType=\"http://www.w3.org/2001/XMLSchema#string\">read</xacml:AttributeValue>\n" +
-                "            <xacml:AttributeDesignator AttributeId=\"urn:oasis:names:tc:xacml:1.0:action:action-id\" Category=\"urn:oasis:names:tc:xacml:3.0:attribute-category:action\" DataType=\"http://www.w3.org/2001/XMLSchema#string\" MustBePresent=\"false\" />\n" +
-                "          </xacml:Match>\n" +
-                "        </xacml:AllOf>\n" +
-                "        <xacml:AllOf>\n" +
-                "          <xacml:Match MatchId=\"urn:oasis:names:tc:xacml:3.0:function:string-equal-ignore-case\">\n" +
-                "            <xacml:AttributeValue DataType=\"http://www.w3.org/2001/XMLSchema#string\">subscribe</xacml:AttributeValue>\n" +
-                "            <xacml:AttributeDesignator AttributeId=\"urn:oasis:names:tc:xacml:1.0:action:action-id\" Category=\"urn:oasis:names:tc:xacml:3.0:attribute-category:action\" DataType=\"http://www.w3.org/2001/XMLSchema#string\" MustBePresent=\"false\" />\n" +
-                "          </xacml:Match>\n" +
-                "        </xacml:AllOf>\n" +
-                "      </xacml:AnyOf>\n" +
-                "    </xacml:Target>\n" +
-                "  </xacml:Rule>\n" +
-                "  <xacml:Rule RuleId=\"urn:altinn:resource:eformidling-meldingsteneste-test:ruleid:3\" Effect=\"Permit\">\n" +
-                "    <xacml:Description>Digdir skal ha tilgang til å sende meldingar</xacml:Description>\n" +
-                "    <xacml:Target>\n" +
-                "<!-- Må ha urn:altinn:resource = eformidling-meldingsteneste-test og urn:altinn:org = digdir -->\n" +
-                "      <xacml:AnyOf>\n" +
-                "        <xacml:AllOf>\n" +
-                "          <xacml:Match MatchId=\"urn:oasis:names:tc:xacml:1.0:function:string-equal\">\n" +
-                "            <xacml:AttributeValue DataType=\"http://www.w3.org/2001/XMLSchema#string\">eformidling-meldingsteneste-test</xacml:AttributeValue>\n" +
-                "            <xacml:AttributeDesignator AttributeId=\"urn:altinn:resource\" Category=\"urn:oasis:names:tc:xacml:3.0:attribute-category:resource\" DataType=\"http://www.w3.org/2001/XMLSchema#string\" MustBePresent=\"false\" />\n" +
-                "          </xacml:Match>\n" +
-                "        </xacml:AllOf>\n" +
-                "      </xacml:AnyOf>\n" +
-                "<!-- Må ha urn:oasis:names:tc:xacml:1.0:action:action-id = read eller (!?) urn:oasis:names:tc:xacml:1.0:action:action-id = subscribe eller (!?) urn:oasis:names:tc:xacml:1.0:action:action-id = write -->\n" +
-                "      <xacml:AnyOf>\n" +
-                "        <xacml:AllOf>\n" +
-                "          <xacml:Match MatchId=\"urn:oasis:names:tc:xacml:3.0:function:string-equal-ignore-case\">\n" +
-                "            <xacml:AttributeValue DataType=\"http://www.w3.org/2001/XMLSchema#string\">read</xacml:AttributeValue>\n" +
-                "            <xacml:AttributeDesignator AttributeId=\"urn:oasis:names:tc:xacml:1.0:action:action-id\" Category=\"urn:oasis:names:tc:xacml:3.0:attribute-category:action\" DataType=\"http://www.w3.org/2001/XMLSchema#string\" MustBePresent=\"false\" />\n" +
-                "          </xacml:Match>\n" +
-                "        </xacml:AllOf>\n" +
-                "        <xacml:AllOf>\n" +
-                "          <xacml:Match MatchId=\"urn:oasis:names:tc:xacml:3.0:function:string-equal-ignore-case\">\n" +
-                "            <xacml:AttributeValue DataType=\"http://www.w3.org/2001/XMLSchema#string\">subscribe</xacml:AttributeValue>\n" +
-                "            <xacml:AttributeDesignator AttributeId=\"urn:oasis:names:tc:xacml:1.0:action:action-id\" Category=\"urn:oasis:names:tc:xacml:3.0:attribute-category:action\" DataType=\"http://www.w3.org/2001/XMLSchema#string\" MustBePresent=\"false\" />\n" +
-                "          </xacml:Match>\n" +
-                "        </xacml:AllOf>\n" +
-                "        <xacml:AllOf>\n" +
-                "          <xacml:Match MatchId=\"urn:oasis:names:tc:xacml:3.0:function:string-equal-ignore-case\">\n" +
-                "            <xacml:AttributeValue DataType=\"http://www.w3.org/2001/XMLSchema#string\">write</xacml:AttributeValue>\n" +
-                "            <xacml:AttributeDesignator AttributeId=\"urn:oasis:names:tc:xacml:1.0:action:action-id\" Category=\"urn:oasis:names:tc:xacml:3.0:attribute-category:action\" DataType=\"http://www.w3.org/2001/XMLSchema#string\" MustBePresent=\"false\" />\n" +
-                "          </xacml:Match>\n" +
-                "        </xacml:AllOf>\n" +
-                "      </xacml:AnyOf>\n" +
-                "      <xacml:AnyOf>\n" +
-                "        <xacml:AllOf>\n" +
-                "          <xacml:Match MatchId=\"urn:oasis:names:tc:xacml:1.0:function:string-equal\">\n" +
-                "            <xacml:AttributeValue DataType=\"http://www.w3.org/2001/XMLSchema#string\">digdir</xacml:AttributeValue>\n" + // NB: dersom test=false ved veksling av token
-                //"            <xacml:AttributeValue DataType=\"http://www.w3.org/2001/XMLSchema#string\">ttd</xacml:AttributeValue>\n" + // NB: dersom test=true ved veksling av token
-                "            <xacml:AttributeDesignator AttributeId=\"urn:altinn:org\" Category=\"urn:oasis:names:tc:xacml:1.0:subject-category:access-subject\" DataType=\"http://www.w3.org/2001/XMLSchema#string\" MustBePresent=\"false\" />\n" +
-                "          </xacml:Match>\n" +
-                "        </xacml:AllOf>\n" +
-                "      </xacml:AnyOf>\n" +
-                "    </xacml:Target>\n" +
-                "  </xacml:Rule>\n" +
-                "  <xacml:ObligationExpressions>\n" +
-                "    <xacml:ObligationExpression ObligationId=\"urn:altinn:obligation:authenticationLevel1\" FulfillOn=\"Permit\">\n" +
-                "      <xacml:AttributeAssignmentExpression AttributeId=\"urn:altinn:obligation1-assignment1\" Category=\"urn:altinn:minimum-authenticationlevel\">\n" +
-                "        <xacml:AttributeValue DataType=\"http://www.w3.org/2001/XMLSchema#integer\">3</xacml:AttributeValue>\n" +
-                "      </xacml:AttributeAssignmentExpression>\n" +
-                "    </xacml:ObligationExpression>\n" +
-                "    <xacml:ObligationExpression ObligationId=\"urn:altinn:obligation:authenticationLevel2\" FulfillOn=\"Permit\">\n" +
-                "      <xacml:AttributeAssignmentExpression AttributeId=\"urn:altinn:obligation2-assignment2\" Category=\"urn:altinn:minimum-authenticationlevel-org\">\n" +
-                "        <xacml:AttributeValue DataType=\"http://www.w3.org/2001/XMLSchema#integer\">3</xacml:AttributeValue>\n" +
-                "      </xacml:AttributeAssignmentExpression>\n" +
-                "    </xacml:ObligationExpression>\n" +
-                "  </xacml:ObligationExpressions>\n" +
-                "</xacml:Policy>";
+        // NB: dersom test=false ved veksling av token
+        //"            <xacml:AttributeValue DataType=\"http://www.w3.org/2001/XMLSchema#string\">ttd</xacml:AttributeValue>\n" + // NB: dersom test=true ved veksling av token
+        String xacml = """
+            <?xml version="1.0" encoding="utf-8"?>
+            <xacml:Policy PolicyId="urn:altinn:policyid:1" Version="1.0" RuleCombiningAlgId="urn:oasis:names:tc:xacml:3.0:rule-combining-algorithm:deny-overrides" xmlns:xacml="urn:oasis:names:tc:xacml:3.0:core:schema:wd-17">
+              <xacml:Target />
+              <xacml:Rule RuleId="urn:altinn:resource:eformidling-meldingsteneste-test:ruleid:2" Effect="Permit">
+                <xacml:Description>Post/arkiv og privatperson skal ha tilgang til å lese og abonnere på meldinger</xacml:Description>
+                <xacml:Target>
+                  <xacml:AnyOf>
+                    <xacml:AllOf>
+                      <xacml:Match MatchId="urn:oasis:names:tc:xacml:3.0:function:string-equal-ignore-case">
+                        <xacml:AttributeValue DataType="http://www.w3.org/2001/XMLSchema#string">a0236</xacml:AttributeValue>
+                        <xacml:AttributeDesignator AttributeId="urn:altinn:rolecode" Category="urn:oasis:names:tc:xacml:1.0:subject-category:access-subject" DataType="http://www.w3.org/2001/XMLSchema#string" MustBePresent="false" />
+                      </xacml:Match>
+                    </xacml:AllOf>
+                    <xacml:AllOf>
+                      <xacml:Match MatchId="urn:oasis:names:tc:xacml:3.0:function:string-equal-ignore-case">
+                        <xacml:AttributeValue DataType="http://www.w3.org/2001/XMLSchema#string">priv</xacml:AttributeValue>
+                        <xacml:AttributeDesignator AttributeId="urn:altinn:rolecode" Category="urn:oasis:names:tc:xacml:1.0:subject-category:access-subject" DataType="http://www.w3.org/2001/XMLSchema#string" MustBePresent="false" />
+                      </xacml:Match>
+                    </xacml:AllOf>
+                  </xacml:AnyOf>
+                  <xacml:AnyOf>
+                    <xacml:AllOf>
+                      <xacml:Match MatchId="urn:oasis:names:tc:xacml:1.0:function:string-equal">
+                        <xacml:AttributeValue DataType="http://www.w3.org/2001/XMLSchema#string">eformidling-meldingsteneste-test</xacml:AttributeValue>
+                        <xacml:AttributeDesignator AttributeId="urn:altinn:resource" Category="urn:oasis:names:tc:xacml:3.0:attribute-category:resource" DataType="http://www.w3.org/2001/XMLSchema#string" MustBePresent="false" />
+                      </xacml:Match>
+                    </xacml:AllOf>
+                  </xacml:AnyOf>
+                  <xacml:AnyOf>
+                    <xacml:AllOf>
+                      <xacml:Match MatchId="urn:oasis:names:tc:xacml:3.0:function:string-equal-ignore-case">
+                        <xacml:AttributeValue DataType="http://www.w3.org/2001/XMLSchema#string">read</xacml:AttributeValue>
+                        <xacml:AttributeDesignator AttributeId="urn:oasis:names:tc:xacml:1.0:action:action-id" Category="urn:oasis:names:tc:xacml:3.0:attribute-category:action" DataType="http://www.w3.org/2001/XMLSchema#string" MustBePresent="false" />
+                      </xacml:Match>
+                    </xacml:AllOf>
+                    <xacml:AllOf>
+                      <xacml:Match MatchId="urn:oasis:names:tc:xacml:3.0:function:string-equal-ignore-case">
+                        <xacml:AttributeValue DataType="http://www.w3.org/2001/XMLSchema#string">subscribe</xacml:AttributeValue>
+                        <xacml:AttributeDesignator AttributeId="urn:oasis:names:tc:xacml:1.0:action:action-id" Category="urn:oasis:names:tc:xacml:3.0:attribute-category:action" DataType="http://www.w3.org/2001/XMLSchema#string" MustBePresent="false" />
+                      </xacml:Match>
+                    </xacml:AllOf>
+                  </xacml:AnyOf>
+                </xacml:Target>
+              </xacml:Rule>
+              <xacml:Rule RuleId="urn:altinn:resource:eformidling-meldingsteneste-test:ruleid:3" Effect="Permit">
+                <xacml:Description>Digdir skal ha tilgang til å sende meldingar</xacml:Description>
+                <xacml:Target>
+            <!-- Må ha urn:altinn:resource = eformidling-meldingsteneste-test og urn:altinn:org = digdir -->
+                  <xacml:AnyOf>
+                    <xacml:AllOf>
+                      <xacml:Match MatchId="urn:oasis:names:tc:xacml:1.0:function:string-equal">
+                        <xacml:AttributeValue DataType="http://www.w3.org/2001/XMLSchema#string">eformidling-meldingsteneste-test</xacml:AttributeValue>
+                        <xacml:AttributeDesignator AttributeId="urn:altinn:resource" Category="urn:oasis:names:tc:xacml:3.0:attribute-category:resource" DataType="http://www.w3.org/2001/XMLSchema#string" MustBePresent="false" />
+                      </xacml:Match>
+                    </xacml:AllOf>
+                  </xacml:AnyOf>
+            <!-- Må ha urn:oasis:names:tc:xacml:1.0:action:action-id = read eller (!?) urn:oasis:names:tc:xacml:1.0:action:action-id = subscribe eller (!?) urn:oasis:names:tc:xacml:1.0:action:action-id = write -->
+                  <xacml:AnyOf>
+                    <xacml:AllOf>
+                      <xacml:Match MatchId="urn:oasis:names:tc:xacml:3.0:function:string-equal-ignore-case">
+                        <xacml:AttributeValue DataType="http://www.w3.org/2001/XMLSchema#string">read</xacml:AttributeValue>
+                        <xacml:AttributeDesignator AttributeId="urn:oasis:names:tc:xacml:1.0:action:action-id" Category="urn:oasis:names:tc:xacml:3.0:attribute-category:action" DataType="http://www.w3.org/2001/XMLSchema#string" MustBePresent="false" />
+                      </xacml:Match>
+                    </xacml:AllOf>
+                    <xacml:AllOf>
+                      <xacml:Match MatchId="urn:oasis:names:tc:xacml:3.0:function:string-equal-ignore-case">
+                        <xacml:AttributeValue DataType="http://www.w3.org/2001/XMLSchema#string">subscribe</xacml:AttributeValue>
+                        <xacml:AttributeDesignator AttributeId="urn:oasis:names:tc:xacml:1.0:action:action-id" Category="urn:oasis:names:tc:xacml:3.0:attribute-category:action" DataType="http://www.w3.org/2001/XMLSchema#string" MustBePresent="false" />
+                      </xacml:Match>
+                    </xacml:AllOf>
+                    <xacml:AllOf>
+                      <xacml:Match MatchId="urn:oasis:names:tc:xacml:3.0:function:string-equal-ignore-case">
+                        <xacml:AttributeValue DataType="http://www.w3.org/2001/XMLSchema#string">write</xacml:AttributeValue>
+                        <xacml:AttributeDesignator AttributeId="urn:oasis:names:tc:xacml:1.0:action:action-id" Category="urn:oasis:names:tc:xacml:3.0:attribute-category:action" DataType="http://www.w3.org/2001/XMLSchema#string" MustBePresent="false" />
+                      </xacml:Match>
+                    </xacml:AllOf>
+                  </xacml:AnyOf>
+                  <xacml:AnyOf>
+                    <xacml:AllOf>
+                      <xacml:Match MatchId="urn:oasis:names:tc:xacml:1.0:function:string-equal">
+                        <xacml:AttributeValue DataType="http://www.w3.org/2001/XMLSchema#string">digdir</xacml:AttributeValue>
+                        <xacml:AttributeDesignator AttributeId="urn:altinn:org" Category="urn:oasis:names:tc:xacml:1.0:subject-category:access-subject" DataType="http://www.w3.org/2001/XMLSchema#string" MustBePresent="false" />
+                      </xacml:Match>
+                    </xacml:AllOf>
+                  </xacml:AnyOf>
+                </xacml:Target>
+              </xacml:Rule>
+              <xacml:ObligationExpressions>
+                <xacml:ObligationExpression ObligationId="urn:altinn:obligation:authenticationLevel1" FulfillOn="Permit">
+                  <xacml:AttributeAssignmentExpression AttributeId="urn:altinn:obligation1-assignment1" Category="urn:altinn:minimum-authenticationlevel">
+                    <xacml:AttributeValue DataType="http://www.w3.org/2001/XMLSchema#integer">3</xacml:AttributeValue>
+                  </xacml:AttributeAssignmentExpression>
+                </xacml:ObligationExpression>
+                <xacml:ObligationExpression ObligationId="urn:altinn:obligation:authenticationLevel2" FulfillOn="Permit">
+                  <xacml:AttributeAssignmentExpression AttributeId="urn:altinn:obligation2-assignment2" Category="urn:altinn:minimum-authenticationlevel-org">
+                    <xacml:AttributeValue DataType="http://www.w3.org/2001/XMLSchema#integer">3</xacml:AttributeValue>
+                  </xacml:AttributeAssignmentExpression>
+                </xacml:ObligationExpression>
+              </xacml:ObligationExpressions>
+            </xacml:Policy>""";
 
         String multipartBoundary = "asdf";
         HttpEntity xacmlHttpEntity = MultipartEntityBuilder.create()
