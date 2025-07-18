839ba842cfba9eab5e36313917202a14a027ba58
Randi Eggum <randi.eggum@digdir.no>
2025-07-18 11:50:34 +0200

    Added test class where we can manually update configuration on altinn 3 resource

--- altinn-v3-client/README.md
+++ altinn-v3-client/README.md
@@ -6,8 +6,11 @@ Beskrive svakheter med kanal begrep i altinn v3 https://docs.digdir.no/docs/eFor
 - må hente ned details for hver enkelt for å se sender reference (så potensielt 101 kall)
 - dersom det er flere IP instanser med hver sin kanal, så kan vil jo 100 meldinger til kanal A blokkere for melding 101 som skal til kanal B
 - så viktig at de som bruker kanal sørger for at alle kanaler faktisk tømmes
+- Få innsyn i korleis DPO tjenesten var konfigurert i altinn 2 og oppdatere ressursen i altinn 3 til å samsvare med konfigurasjonen.
 
 ## DPO
+
+ManualResourceTest.java kan brukes for å konfigurere instillinger på ressursen til DPO.
 
 ## DPV
 
---
+++ altinn-v3-client/src/test/java/no/difi/meldingsutveksling/altinnv3/DPO/ManualResourceTest.java
@@ -0,0 +1,57 @@
+package no.difi.meldingsutveksling.altinnv3.DPO;
+
+import com.nimbusds.jose.JOSEException;
+import no.difi.meldingsutveksling.altinnv3.ApiUtils;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.client.RestClient;
+
+import java.io.IOException;
+
+@Disabled
+public class ManualResourceTest {
+    @Test
+    public void getResource() throws IOException, InterruptedException, JOSEException {
+        String accessToken = ApiUtils.retrieveAccessToken("altinn:broker.write altinn:broker.read altinn:serviceowner");
+
+        RestClient restClient = RestClient.create();
+        var result = restClient.get()
+            .uri("https://platform.tt02.altinn.no/broker/api/v1/resource/meldingsutveksling_dpo")
+            .header("Authorization", "Bearer " + accessToken)
+            .header("Accept", "application/json")
+            .retrieve()
+            .body(String.class)
+            ;
+
+        System.out.println(result);
+    }
+
+    @Test
+    public void updateResource() throws IOException, InterruptedException, JOSEException {
+        String accessToken = ApiUtils.retrieveAccessToken("altinn:broker.write altinn:broker.read altinn:serviceowner");
+
+        String body = """
+            {
+                "resourceId": "meldingsutveksling_dpo",
+                "maxFileTransferSize": "1073741824",
+                "fileTransferTimeToLive": "P30D",
+                "PurgeFileTransferAfterAllRecipientsConfirmed" : true,
+                "purgeFileTransferGracePeriod": "PT24H",
+                "ExternalServiceCodeLegacy": "4192",
+                "ExternalServiceEditionCodeLegacy": "270815",
+                "UseManifestFileShim": true
+            }
+        """;
+
+        RestClient restClient = RestClient.create();
+        restClient.put()
+            .uri("https://platform.tt02.altinn.no/broker/api/v1/resource/meldingsutveksling_dpo")
+            .header("Authorization", "Bearer " + accessToken)
+            .header("Accept", "application/json")
+            .header("Content-Type", "application/json")
+            .body(body)
+            .retrieve()
+            .toBodilessEntity()
+            ;
+    }
+}
