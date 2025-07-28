3a1091338b1f00a3663f9fbc848aa5c6d31bb538
Randi Eggum <randi.eggum@digdir.no>
2025-07-28 10:21:03 +0200

    Remove validation of empty username and password in dpv settings. Username and password are not used in altinn 3

--- common-spring/src/main/java/no/difi/meldingsutveksling/config/IntegrasjonspunktPropertiesValidator.java
+++ common-spring/src/main/java/no/difi/meldingsutveksling/config/IntegrasjonspunktPropertiesValidator.java
@@ -8,7 +8,6 @@ public class IntegrasjonspunktPropertiesValidator implements Validator {
 
     private static final String EMPTY_FIELD = "empty_field";
     private static final String DPFIO_ERROR_MSG = "DPFIO enabled - cannot be null";
-    private static final String DPV_ERROR_MSG = "DPV enabled - cannot be null";
 
     @Override
     public boolean supports(Class<?> clazz) {
@@ -18,11 +17,6 @@ public class IntegrasjonspunktPropertiesValidator implements Validator {
     @Override
     public void validate(Object target, Errors errors) {
         IntegrasjonspunktProperties props = (IntegrasjonspunktProperties) target;
-
-        if (props.getFeature().isEnableDPV()) {
-            ValidationUtils.rejectIfEmpty(errors, "dpv.username", EMPTY_FIELD, DPV_ERROR_MSG);
-            ValidationUtils.rejectIfEmpty(errors, "dpv.password", EMPTY_FIELD, DPV_ERROR_MSG);
-        }
 
         if (props.getFeature().isEnableDPFIO()) {
             ValidationUtils.rejectIfEmpty(errors, "fiks.io.konto-id", EMPTY_FIELD, DPFIO_ERROR_MSG);
