ba9bd03a618e30cf92f3d73be8e2b24fd41d71cf
Alexander Petrov <alexander.petrov@digdir.no>
2025-08-29 15:40:39 +0200

    fiks compilation fail

--- integrasjonspunkt/src/test/java/no/difi/meldingsutveksling/nextmove/dph/DPHMessageOutTest.java
+++ integrasjonspunkt/src/test/java/no/difi/meldingsutveksling/nextmove/dph/DPHMessageOutTest.java
@@ -2,6 +2,7 @@ package no.difi.meldingsutveksling.nextmove.dph;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import no.difi.meldingsutveksling.nextmove.nhn.DPHMessageOut;
+import no.difi.meldingsutveksling.nextmove.nhn.Patient;
 import no.difi.meldingsutveksling.nextmove.nhn.Reciever;
 import no.difi.meldingsutveksling.nextmove.nhn.Sender;
 import org.junit.jupiter.api.Test;
@@ -11,7 +12,14 @@ public class DPHMessageOutTest {
 
     @Test
     public void testDphSerialization() throws Exception {
-        DPHMessageOut testMessageOut = new DPHMessageOut("testMessageId","testConversationId", new Sender("testHerid1","testHerid2"), new Reciever("testHerid1","testHerid2"),"testfagmelding");
+        DPHMessageOut testMessageOut = new DPHMessageOut("testMessageId",
+            "testConversationId",
+            "234234234",
+            new Sender("testHerid1","testHerid2"),
+            new Reciever("testHerid1","testHerid2"),
+            "testfagmelding",
+            new Patient("234123421341234","Peter","","Peterson","45345345435")
+        );
 
         ObjectMapper mapper = new ObjectMapper();
         var json = mapper.writeValueAsString(testMessageOut);
