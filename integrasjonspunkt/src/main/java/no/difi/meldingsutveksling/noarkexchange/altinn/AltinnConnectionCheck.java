482a66326be3e247d510913f0fb7ea1a9009683d
Randi Eggum <randi.eggum@digdir.no>
2025-11-11 15:43:47 +0100

    Updated AltinnConnectionCheck to Altinn 3 from Altinn 2

--- integrasjonspunkt/src/main/java/no/difi/meldingsutveksling/noarkexchange/altinn/AltinnConnectionCheck.java
+++ integrasjonspunkt/src/main/java/no/difi/meldingsutveksling/noarkexchange/altinn/AltinnConnectionCheck.java
@@ -1,12 +1,16 @@
 package no.difi.meldingsutveksling.noarkexchange.altinn;
 
+import com.google.common.collect.Sets;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import no.difi.meldingsutveksling.altinnv3.dpo.BrokerApiClient;
 import no.difi.meldingsutveksling.config.IntegrasjonspunktProperties;
 import no.difi.meldingsutveksling.domain.MeldingsUtvekslingRuntimeException;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
 
 @RequiredArgsConstructor
 @Slf4j
@@ -14,18 +18,24 @@ import org.springframework.stereotype.Component;
 @Component
 public class AltinnConnectionCheck {
 
-//    private final AltinnWsClient altinnWsClient;
-    private final IntegrasjonspunktProperties props;
+    private final IntegrasjonspunktProperties properties;
+    private final BrokerApiClient brokerApiClient;
 
     @PostConstruct
     public void checkTheConnection() {
-        try {
-            // FIXME gjør en "ping" sjekk for å se om det er mulig å snakke med AltInn v3
-            //altinnWsClient.checkIfAvailableFiles(props.getOrg().getNumber());
-            // TODO verify integrity of difi.move.dpo.reportees delegation
-        } catch (Exception e) {
-            throw new MeldingsUtvekslingRuntimeException("Could not connect to Altinn", e);
-        }
-    }
+        var systemUsers = Sets.newHashSet(properties.getDpo().getSystemUser());
+        systemUsers.addAll(properties.getDpo().getReportees());
 
+        systemUsers.forEach(systemUser -> {
+            try {
+                brokerApiClient.getAvailableFiles(systemUser);
+            }
+            catch (Exception e) {
+                if(e instanceof HttpClientErrorException ex && ex.getStatusCode() == HttpStatus.NOT_FOUND) {
+                    throw new MeldingsUtvekslingRuntimeException("Could not connect to Altinn, got 404 not found, systemuser " + systemUser.getName() + " might not exist", e);
+                }
+                throw new MeldingsUtvekslingRuntimeException("Could not connect to Altinn with systemUser.orgId: " + systemUser.getOrgId() + " systemUser.name: " + systemUser.getName(), e);
+            }
+        });
+    }
 }
