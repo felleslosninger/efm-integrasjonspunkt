01b475cc8cce8a0d8dc4fd0e0051a83d0c48e976
Lars Bardgard Astveit <lab@digdir.no>
2025-12-08 13:59:09 +0100

    MOVE-4732 Use SerialBlob instead of EntityManager blob to avid bug where IP writes to disk, add Transactional flag Propagation.REQUIRES_NEW to read blob from database, write transaction blocks read...

--- integrasjonspunkt/src/main/java/no/difi/meldingsutveksling/nextmove/message/DBMessagePersister.java
+++ integrasjonspunkt/src/main/java/no/difi/meldingsutveksling/nextmove/message/DBMessagePersister.java
@@ -7,6 +7,7 @@ import no.difi.meldingsutveksling.nextmove.NextMoveMessageEntry;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import jakarta.persistence.PersistenceException;
@@ -34,7 +35,7 @@ public class DBMessagePersister implements MessagePersister {
     }
 
     @Override
-    @Transactional(readOnly = true)
+    @Transactional(readOnly = true, propagation = Propagation.REQUIRES_NEW)
     public Resource read(String messageId, String filename) throws IOException {
         NextMoveMessageEntry entry = repo.findByMessageIdAndFilename(messageId, filename).findFirst()
                 .orElseThrow(() -> new PersistenceException("Entry for conversationId=%s, filename=%s not found in database".formatted(messageId, filename)));
