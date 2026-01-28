01727b14a1fac93aecea9ead85c493d34244efd7
Alexander Petrov <alexander.petrov@digdir.no>
2026-01-28 13:31:18 +0100

    MOVE-4251 review kommentarer

--- service-registry-client/src/main/java/no/difi/meldingsutveksling/serviceregistry/client/ServiceRegistryRestClient.java
+++ service-registry-client/src/main/java/no/difi/meldingsutveksling/serviceregistry/client/ServiceRegistryRestClient.java
@@ -12,8 +12,6 @@ import java.net.URI;
 import java.util.Collections;
 import java.util.Map;
 
-import static org.springframework.http.HttpHeaders.ACCEPT;
-
 /**
  * RestClient using simple http requests to manipulate services
  * <p>
@@ -26,11 +24,8 @@ import static org.springframework.http.HttpHeaders.ACCEPT;
 @RequiredArgsConstructor
 public class ServiceRegistryRestClient {
 
-    static final String X_ENABLE_BETA_FEATURES = "X-Enable-Beta-Features";
-
     private final IntegrasjonspunktProperties props;
-    @Getter
-    private final RestClient restClient;
+    @Getter private final RestClient restClient;
     private final JWTDecoder jwtDecoder;
     private final URI baseUrl;
 
@@ -47,24 +42,11 @@ public class ServiceRegistryRestClient {
     public String getResource(String urlTemplate, Map<String, String> urlVariables) throws BadJWSException {
         UriTemplate uriTemplate = new UriTemplate(baseUrl.toString().concat("/").concat(urlTemplate));
         var uri = uriTemplate.expand(urlVariables);
-        RestClient.RequestHeadersSpec<?> spec = restClient.get().uri(uri);
-
-        if (props.getFeature().isEnableBetaFeatures()) {
-            spec = spec.header(X_ENABLE_BETA_FEATURES, "true");
-        }
-
         if (props.getSign().isEnable()) {
-            var body = spec
-                .header(ACCEPT, "application/jose", "application/json")
-                .retrieve()
-                .toEntity(String.class)
-                .getBody();
+            var body = restClient.get().uri(uri).header("Accept", "application/jose", "application/json").header("X-Enable-Beta-Features","true").retrieve().toEntity(String.class).getBody();
             return jwtDecoder.getPayload(body, props.getSign().getJwkUrl());
         }
-
-        return spec
-            .retrieve()
-            .toEntity(String.class)
-            .getBody();
+        return restClient.get().uri(uri).retrieve().toEntity(String.class).getBody();
     }
+
 }
