06ed532b9863fa777de48afa35c48ca256cda134
Randi Eggum <randi.eggum@digdir.no>
2026-04-17 15:29:26 +0200

    Added a connection check for DPO reportees, so we fail fast if there is an error in the reportee setup

--- common-spring/src/main/java/no/difi/meldingsutveksling/config/AltinnSystemUser.java
+++ common-spring/src/main/java/no/difi/meldingsutveksling/config/AltinnSystemUser.java
@@ -1,5 +1,6 @@
 package no.difi.meldingsutveksling.config;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,10 +13,12 @@ public class AltinnSystemUser {
     /**
      * format : 0192:311780735
      */
+    @NotNull
     private String orgId;
     /**
      * eks : 311780735_integrasjonspunkt_systembruker_test
      */
+    @NotNull
     private String name;
 
 }
