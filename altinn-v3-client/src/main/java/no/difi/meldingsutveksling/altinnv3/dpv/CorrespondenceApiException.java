1d3fef852371b42fd91bff5fb2346337a9df0158
Thore Johnsen <thore@jpro.no>
2026-07-28 10:35:22 +0200

    Fanger client error og kaster Queue exception som vil fjerne fra kø

--- altinn-v3-client/src/main/java/no/difi/meldingsutveksling/altinnv3/dpv/CorrespondenceApiException.java
+++ altinn-v3-client/src/main/java/no/difi/meldingsutveksling/altinnv3/dpv/CorrespondenceApiException.java
@@ -1,17 +1,31 @@
 package no.difi.meldingsutveksling.altinnv3.dpv;
 
+import lombok.Getter;
+import org.springframework.http.HttpStatusCode;
+
+@Getter
 public class CorrespondenceApiException extends RuntimeException {
+
+    private final HttpStatusCode statusCode;
 
     public CorrespondenceApiException() {
         super();
+        this.statusCode = null;
     }
 
     public CorrespondenceApiException(String message) {
         super(message);
+        this.statusCode = null;
     }
 
     public CorrespondenceApiException(String message, Throwable cause) {
         super(message, cause);
+        this.statusCode = null;
+    }
+
+    public CorrespondenceApiException(String message, HttpStatusCode statusCode) {
+        super(message);
+        this.statusCode = statusCode;
     }
 
 }
--- common/src/main/java/no/difi/meldingsutveksling/QueueInterruptException.java
+++ common/src/main/java/no/difi/meldingsutveksling/QueueInterruptException.java
@@ -1,9 +1,23 @@
 package no.difi.meldingsutveksling;
 
+import lombok.Getter;
+
+@Getter
 public class QueueInterruptException extends RuntimeException {
+
+    private final Integer httpCode;
+    private final boolean clientError;
 
     public QueueInterruptException(String message) {
         super(message);
+        this.httpCode = null;
+        this.clientError = false;
+    }
+
+    public QueueInterruptException(String message, int httpCode, boolean clientError) {
+        super(message);
+        this.httpCode = httpCode;
+        this.clientError = clientError;
     }
 
 }
