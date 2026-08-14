f340377b843fc6abb05bf409e47cd4af441f9b90
Håvard England <hme@digdir.no>
2026-08-14 10:31:02 +0200

    MOVE-5120 Bygg testmapperen med customizer-bønnene

--- integrasjonspunkt/src/test/java/no/difi/meldingsutveksling/nextmove/v2/JacksonTestConfig.java
+++ integrasjonspunkt/src/test/java/no/difi/meldingsutveksling/nextmove/v2/JacksonTestConfig.java
@@ -2,7 +2,9 @@ package no.difi.meldingsutveksling.nextmove.v2;
 
 import no.difi.meldingsutveksling.clock.ClockConfig;
 import no.difi.meldingsutveksling.config.JacksonConfig;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.jackson.autoconfigure.JsonMapperBuilderCustomizer;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
@@ -14,9 +16,16 @@ import tools.jackson.databind.json.JsonMapper;
 @Import({JacksonConfig.class, ClockConfig.class})
 public class JacksonTestConfig {
 
+    /**
+     * Test-slices (@DataJpaTest o.l.) autokonfigurerer ikkje Jackson, så mapperen må byggast
+     * her. Customizer-bønnene blir brukte, slik at testane får same mapper-oppsett som
+     * produksjon (jf. {@link JacksonConfig}).
+     */
     @Bean
     @ConditionalOnMissingBean
-    public JsonMapper jsonMapper() {
-        return JsonMapper.builder().build();
+    public JsonMapper jsonMapper(ObjectProvider<JsonMapperBuilderCustomizer> customizers) {
+        JsonMapper.Builder builder = JsonMapper.builder();
+        customizers.orderedStream().forEach(customizer -> customizer.customize(builder));
+        return builder.build();
     }
 }
